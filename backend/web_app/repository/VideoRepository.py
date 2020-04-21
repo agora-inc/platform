8c3b8d74ba071c499634f0389bd8775f623e19e9
Max Taylor-Davies <maxtaylordavies@gmail.com>
2020-04-21 16:09:24 +0100

    updates get-videos-by-tag endpoint to sort by date descending

--- backend/web_app/repository/VideoRepository.py
+++ backend/web_app/repository/VideoRepository.py
@@ -57,6 +57,6 @@ class VideoRepository:
             return []
          
         tagId = result[0]["id"]
-        cursor.execute('SELECT Videos.id, Videos.channel_id, Videos.name, Videos.description, Videos.date, Videos.views FROM Videos INNER JOIN VideoTags ON Videos.id = VideoTags.video_id WHERE VideoTags.tag_id = %d' % tagId)
+        cursor.execute('SELECT Videos.id, Videos.channel_id, Videos.name, Videos.description, Videos.date, Videos.views FROM Videos INNER JOIN VideoTags ON Videos.id = VideoTags.video_id WHERE VideoTags.tag_id = %d ORDER BY Videos.date DESC' % tagId)
         result = cursor.fetchall()
         return result
