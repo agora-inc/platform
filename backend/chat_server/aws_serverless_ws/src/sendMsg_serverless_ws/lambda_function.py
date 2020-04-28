0bfe31c046458507e7ae3e16b533cfb7a3c99357
remymess <remy.messadene@gmail.com>
2020-04-28 05:01:42 +0200

    Partial commit: all logic implemented. Remains to test.

--- backend/chat_server/aws_serverless_ws/src/sendMsg_serverless_ws/lambda_function.py
+++ backend/chat_server/aws_serverless_ws/src/sendMsg_serverless_ws/lambda_function.py
@@ -2,34 +2,72 @@ import pymysql
 import logging
 import rds_config
 import sys
-
-from serverless_db import db 
+import requests
 
 # logger
 logger = logging.getLogger()
 logger.info("SUCCESS: Connection to RDS instance succeeded")
 
 # DB
-class Database:
-    def __init__(self):
-        host = rds_config.host
-        user = rds_config.user
-        password = rds_config.password
-        db_name = rds_config.name
-        self.con = pymysql.connect(host=host, user=user, password=password, db=db_name, curso$
-                                   DictCursor)
+host = rds_config.host
+user = rds_config.user
+password = rds_config.password
+db_name = rds_config.name
+con = pymysql.connect(host=host, user=user, password=password, db=db_name, cursorclass=pymysql.cursors.DictCursor)
 
+def _wrapper(status_code, body, header={}):
+    return {"statusCode": status_code, "body": body}
 
 def handler(event, context):
     """
     This function fetches content from RDS instance
     """
-
     try:
-        # get "RemoteAddr" from the HTTP request
-        # get list of all adresses in DB
-        # send msg to all the addresses
+        # get "RemoteAddr" from the HTTP request and add it in the DB
+        ip_address = event["headers"]["X-Forwarded-For"]
 
-    except:
+        assert("chat_participant_group_id" in event)
+        chat_participant_group_id = event["chat_participant_group_id"]
 
-        return {"statusCode": 200, "data": event["action"]}
+        if "user_id" in event:
+            user_id = event["user_id"]
+        else:
+            user_id = "NULL"
+
+        body = {
+            "msg": event["msg"],
+            "action": "receiveMsg",
+            "group_type": event["group_type"],
+            "chat_participant_group_id": chat_participant_group_id,
+            "source_user_id": user_id
+            }
+    
+        # A) if target_user_id, one-one conversation
+        if "target_user_id" in event:
+            cursor = con.cursor()
+            cursor.execute(f"SELECT ip_address FROM OnlineUsers WHERE user_id={event[target_user_id]}")
+            res = cursor.fetchall()
+
+            for ip in res:
+                requests.post(url=ip, body=body)
+
+        # B) else, this is a msg to broadcast
+        else:
+            cursor.execute(f"SELECT ip_address FROM ChatParticipantGroups WHERE chat_id={chat_id} and chat_type='{event['group_type']}'")
+            ips = cursor.fetchall()
+
+            for i in ips:
+                request.post(url=i["ip"], body=body)
+
+        return _wrapper(200, body)
+    
+    except Exception as e:
+        return _wrapper(500, f"Exception: {e}")
+
+
+# if __name__ == "__main__":
+#     #############
+#     # TEST UNIT: #
+#     #############
+#     event_example = {"chat_id": 16, "headers": {"X-Forwarded-For": "192.168.1.1"}}
+#     print(handler(event_example, {}))
