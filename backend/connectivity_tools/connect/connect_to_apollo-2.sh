ae7753967c6e4f7701ca81c5e48f468fb0cd76dd
remymess <remy.messadene@gmail.com>
2020-04-28 05:04:40 +0200

    Script for almost instant logging on db

---
+++ backend/connectivity_tools/connect/connect_to_apollo-2.sh
@@ -0,0 +1,2 @@
+mysql -h apollo-2.c91ghtqneybi.eu-west-2.rds.amazonaws.com -P 3306 -u admin -p
+
---
+++ backend/connectivity_tools/connect/connect_to_database-2.sh
@@ -0,0 +1,1 @@
+mysql -h database-2-chat-ws.c91ghtqneybi.eu-west-2.rds.amazonaws.com -P 3306 -u admin -p
