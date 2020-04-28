08d24bb3fab34fa99d7c373fe9ebe691d1719dfa
remymess <remy.messadene@gmail.com>
2020-04-28 02:27:56 +0200

    Typo

--- backend/chat_server/aws_serverless_ws/build/rds_config.py
+++ backend/chat_server/aws_serverless_ws/build/rds_config.py
@@ -1,8 +1,7 @@
-#config file containing credentials for RDS PostgreSQL instance
+#config file containing credentials for RDS MySQL instance
 host = "apollo-2.c91ghtqneybi.eu-west-2.rds.amazonaws.com"
 user = "admin"
 password = "123.qwe.asd"
 name = "apollo"
-
 
 
