1fb4464b58f938ffbd012f5dc3f0f68c3c2adb28
remymess <remy.messadene@gmail.com>
2020-05-15 02:22:39 +0200

    Adding logistic scripts for ws

--- backend/chat_server/homemade/build/deploy_chat_ws_server.sh
+++ backend/chat_server/homemade/build/deploy_chat_ws_server.sh
@@ -1,1 +1,1 @@
-python /home/cloud-user/plateform/agora/backend/chat_server/homemade/src/chat_ws_server.py
+nohup python /home/cloud-user/plateform/agora/backend/chat_server/homemade/src/chat_ws_server.py &
