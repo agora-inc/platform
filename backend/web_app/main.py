ad54c0f78ba68e666d4864b0d6ece1108ecd3176
remymess <remy.messadene@gmail.com>
2020-06-04 18:53:26 +0200

    adding ssl flask test

--- backend/web_app/main.py
+++ backend/web_app/main.py
@@ -1,4 +1,9 @@
 from app import app, db
+from OpenSSL import SSL
 
 if __name__ == "__main__":
-    app.run(ssl_context='adhoc')
+    key_loc = '/etc/nginx/conf.d/ssl-files/ssl_csr.key'
+    cert_loc = '/etc/nginx/conf.d/ssl-files/www_agora_stream.crt'
+    context = (cert_loc, key_loc)
+
+    app.run(ssl_context=context)
