472a275a007b048ebf72377f3a752237444f9182
remymess <remy.messadene@gmail.com>
2020-06-06 17:40:42 +0000

    restablishing http wsgi

--- backend/web_app/wsgi.py
+++ backend/web_app/wsgi.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     cert_loc = '/etc/nginx/conf.d/ssl-files/www_agora_stream.crt'
     context = (cert_loc, key_loc)
     try:
-        app.run()
+        app.run(host="0.0.0.0", port=8000)
         #app.run(ssl_context=context)
     except:
         app.run(t=8000)
