5a49d70d2cc7d1c1869cc86339035ffaab98d1ae
remymess <remy.messadene@gmail.com>
2020-06-06 19:20:44 +0000

    updates gitignore

--- backend/web_app/wsgi.py
+++ backend/web_app/wsgi.py
@@ -5,8 +5,4 @@ if __name__ == "__main__":
     key_loc = '/etc/nginx/conf.d/ssl-files/ssl_csr.key'
     cert_loc = '/etc/nginx/conf.d/ssl-files/www_agora_stream.crt'
     context = (cert_loc, key_loc)
-    try:
-        app.run(host="0.0.0.0", port=8000)
-        #app.run(ssl_context=context)
-    except:
-        app.run(t=8000)
+    app.run(ssl_context=context, port=8000)
