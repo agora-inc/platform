5bba935d386cfbbc441efc10bd7c92027a5ff847
remymess <remy.messadene@gmail.com>
2020-06-06 17:20:16 +0000

    restablishing working setup in http

--- backend/web_app/wsgi.py
+++ backend/web_app/wsgi.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     key_loc = '/etc/nginx/conf.d/ssl-files/ssl_csr.key'
     cert_loc = '/etc/nginx/conf.d/ssl-files/www_agora_stream.crt'
     context = (cert_loc, key_loc)
-
     try:
-        app.run(ssl_context=context)
+        app.run()
+        #app.run(ssl_context=context)
     except:
-        app.run(port=8000)
+        app.run(t=8000)
