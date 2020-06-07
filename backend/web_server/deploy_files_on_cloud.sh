0d781197af9dadd55db59edcaf16bba55b70d485
remymess <remy.messadene@gmail.com>
2020-06-07 12:46:07 +0000

    updating repo with the working ssl configs

--- backend/web_server/deploy_files_on_cloud.sh
+++ backend/web_server/deploy_files_on_cloud.sh
@@ -1,5 +1,5 @@
 # Agora config
-sudo \cp /home/cloud-user/plateform/agora/backend/web_server/nginx_configs/agora /etc/nginx/sites-enabled/
+sudo \cp /home/cloud-user/plateform/agora/backend/web_server/nginx_configs/agora-https /etc/nginx/sites-enabled/
 sudo \cp -r /home/cloud-user/plateform/agora/backend/web_server/nginx_configs/ssl-files /etc/nginx/conf.d/
 
 # Meta configs
