1446f0b4c4a1ee0ae36a2193bcb308df1c1febf2
Max Taylor-Davies <maxtaylordavies@gmail.com>
2020-06-24 20:18:46 +0100

    adds current time next to timezone info

--- frontend/src/Components/TimeZoneInfo.tsx
+++ frontend/src/Components/TimeZoneInfo.tsx
@@ -1,13 +1,36 @@
-import React, { Component, Fragment } from "react";
-import { Text } from "grommet";
+import React, { Component } from "react";
+import moment from "moment";
 import "../Styles/tooltip.css";
 
-export default class TimeZoneInfo extends Component {
+type State = {
+  time: string;
+};
+
+export default class TimeZoneInfo extends Component<{}, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      time: moment().format("HH:mm"),
+    };
+  }
+
+  componentWillMount() {
+    setInterval(this.updateTime, 1000);
+  }
+
+  updateTime = () => {
+    this.setState({ time: moment().format("HH:mm") });
+  };
+
+  //   componentWillUnmount() {
+  //     clearInterval(this.interval);
+  //   }
+
   render() {
     const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
     return (
       <div className="tooltip" style={{ fontSize: 15, marginRight: 5 }}>
-        {tz}
+        {tz} {this.state.time}
         <span className="tooltiptext">
           When you create or view a talk, the time is shown in your local
           timezone
