5c33b3c2fa06651924d6856ceb39237e0d80e051
Max Taylor-Davies <maxtaylordavies@gmail.com>
2020-06-26 16:00:20 +0100

    adds endpoint for getting all current talks

--- backend/web_app/app/routes.py
+++ backend/web_app/app/routes.py
@@ -209,7 +209,7 @@ def getAllFutureTalks():
 def getAllCurrentTalks():
     limit = int(request.args.get("limit"))
     offset = int(request.args.get("offset"))
-    data = jsonify(talks.getAllCurrentTalks(limit, offset))
+    data = talks.getAllCurrentTalks(limit, offset)
     return jsonify({"talks": data[0],"count": data[1]})
 
 @app.route('/talks/all/past', methods=["GET"])
