8f9a2c00f61e6e8287757c2a8b053c664f8181fd
remymess <remy.messadene@gmail.com>
2020-08-03 00:33:24 +0200

    Fixing alignment LoginCard and Sign Up card (color issue where button where not aligned with colour + not at the bottom of page)

--- frontend/src/Components/Account/LoginModal.tsx
+++ frontend/src/Components/Account/LoginModal.tsx
@@ -95,7 +95,7 @@ export default class LoginModal extends Component<Props, State> {
           <Box
             width="100%"
             height="100%"
-            justify="end"
+            // justify="end"
             align="center"
             // pad={{ bottom: "50px" }}
             gap="xsmall"
