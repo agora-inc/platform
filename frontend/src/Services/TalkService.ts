f2029de8175b5d3263b9dbe3dd3c9f45e31d296e
remymess <remy.messadene@gmail.com>
2020-08-16 19:16:55 +0200

    Updating endpoints in flask + frontend service to incorporate new field for talks ('published' or 'drafted')

--- frontend/src/Services/TalkService.ts
+++ frontend/src/Services/TalkService.ts
@@ -50,6 +50,10 @@ const getPastTalksForChannel = (channelId: number, callback: any) => {
   get(`talks/channel/past?channelId=${channelId}`, callback);
 };
 
+const getDraftedTalksForChannel = (channelId: number, callback: any) => {
+  get(`talks/channel/drafted?channelId=${channelId}`, callback);
+};
+
 const getPastTalksForTag = (tagName: string, callback: any) => {
   get(`talks/tag/past?tagName=${tagName}`, callback);
 };
@@ -67,6 +71,7 @@ const editTalk = (
   topics: Topic[],
   talkSpeaker: string,
   talkSpeakerURL: string,
+  state: string,
   callback: any
 ) => {
   post(
@@ -86,6 +91,7 @@ const editTalk = (
       topic3Id: topics[2] ? topics[2].id : 0,
       talkSpeaker: talkSpeaker,
       talkSpeakerURL: talkSpeakerURL,
+      state: state
     },
     callback
   );
@@ -105,6 +111,7 @@ const scheduleTalk = (
   topics: Topic[],
   talkSpeaker: string,
   talkSpeakerURL: string,
+  state: string,
   callback: any
 ) => {
   post(
@@ -125,6 +132,7 @@ const scheduleTalk = (
       topic3Id: topics.length > 2 ? topics[2].id : null,
       talkSpeaker: talkSpeaker,
       talkSpeakerURL: talkSpeakerURL,
+      state: state
     },
     callback
   );
@@ -229,6 +237,7 @@ export const TalkService = {
   getAllPastTalks,
   getFutureTalksForChannel,
   getPastTalksForChannel,
+  getDraftedTalksForChannel,
   getFutureTalksForTopic,
   getAllFutureTalksForTopicWithChildren,
   getPastTalksForTopic,
