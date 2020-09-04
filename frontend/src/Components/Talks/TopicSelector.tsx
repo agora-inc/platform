23d4aac6c049cc84ee9421d0cfde01b624595b69
AlainRossier <alain6rossier@gmail.com>
2020-09-04 01:04:24 +0100

    add and cancel topics

--- frontend/src/Components/Talks/TopicSelector.tsx
+++ frontend/src/Components/Talks/TopicSelector.tsx
@@ -9,8 +9,7 @@ import Icon from "@ant-design/icons";
 interface State {
   all: Topic[],
   topics: Topic[][];
-  topicsBeingShown: number[];
-  numberTopicsChosen: number;
+  topicsShown: number[];
 }
 
 interface Props {
@@ -24,8 +23,7 @@ export default class TopicSelector extends Component<Props, State> {
     this.state = {
       all: [],
       topics: [[], [], []],
-      topicsBeingShown: [0, 0, 0],
-      numberTopicsChosen: -1,
+      topicsShown: [0, 0, 0],
     };
   }
 
@@ -35,29 +33,46 @@ export default class TopicSelector extends Component<Props, State> {
     });
   }
 
-  onClickAddTopic = () => {
-    this.setState((state) => ({
-      numberTopicsChosen: state.numberTopicsChosen + 1,
-    }));
+  onAddTopicShown = (choice: number) => {
+    return ( () => {
+      let tempTopics = this.state.topics;
+      let tempTopicsShown = this.state.topicsShown;
+      tempTopics[choice] = []
+      tempTopicsShown[choice] = 1;
+      this.setState({ 
+        topicsShown: tempTopicsShown,
+        topics: tempTopics 
+      });
+    })
   };
 
-  onFieldChoose = (choiceNumber: number, topic: Topic, fieldDepth: number) => {
-    let tempBools = this.state.topicsBeingShown;
+  onCancelTopicShown = (choice: number) => {
+    return ( () => {
+      let tempTopics = this.state.topics;
+      let tempShown = this.state.topicsShown;
+      tempTopics[choice] = []
+      tempShown[choice] = 0;
+      this.setState({ 
+        topicsShown: tempShown,
+        topics: tempTopics 
+      });
+    })
+  };
 
+  onFieldChoose = (choice: number, topic: Topic, fieldDepth: number) => {
+    let tempShown = this.state.topicsShown;
     if (topic.id >= 0) {
-      tempBools[choiceNumber] = fieldDepth + 1;
-      this.props.onSelectedCallback(topic, choiceNumber);
+      tempShown[choice] = fieldDepth + 1;
+      this.props.onSelectedCallback(topic, choice);
     } else {
-      tempBools[choiceNumber] = fieldDepth;
-      
+      tempShown[choice] = fieldDepth; 
     }
-
     let tempTopics = this.state.topics;
-    tempTopics[choiceNumber] = tempTopics[choiceNumber].slice(0, fieldDepth);
-    tempTopics[choiceNumber].push(topic);
+    tempTopics[choice] = tempTopics[choice].slice(0, fieldDepth);
+    tempTopics[choice].push(topic);
     this.setState({
       topics: tempTopics,
-      topicsBeingShown: tempBools,
+      topicsShown: tempShown,
     });
   };
 
@@ -112,29 +127,10 @@ export default class TopicSelector extends Component<Props, State> {
       .map((temp: Topic) => temp.field);
   };
 
-  onCancelClick = (choiceNumber: number) => {
-    return (() => {
-        let tempTopics = this.state.topics;
-        tempTopics[choiceNumber] = []; 
-        let tempTopicsBeingShown = this.state.topicsBeingShown;
-        tempTopicsBeingShown[choiceNumber] = 0;
-        // let tempTopics = this.state.topics.splice(choiceNumber, 1);
-        // tempTopics.push([]);
-        // let tempTopicsBeingShown = this.state.topicsBeingShown.splice(choiceNumber, 1);
-        // tempTopicsBeingShown.push(0);
-        this.setState({
-          topics: tempTopics,
-          topicsBeingShown: tempTopicsBeingShown,
-        });
-        // this.setState((state) => ({
-        //  numberTopicsChosen: state.numberTopicsChosen - 1
-        // }));
-      }
-    );
-  };
 
-  renderTopicChoice = (choiceNumber: number) => {
-    if (choiceNumber <= this.state.numberTopicsChosen) {
+
+  renderTopicChoice = (choice: number) => {
+    if (this.state.topicsShown[choice] > 0) {
       return (
         <Box
           width="100%"
@@ -143,66 +139,64 @@ export default class TopicSelector extends Component<Props, State> {
           align="center"
           margin={{ bottom: "15px" }}
         >
-          {this.state.topicsBeingShown[choiceNumber] >= 0 && (
+          {this.state.topicsShown[choice] > 0 && (
             <Select
               searchPlaceholder={"All"}
               options={this.getPrimitiveNodes().concat("All")}
               onChange={({ option }) =>
-                this.onFieldChoose(choiceNumber, this.nameToTopic(option), 0)
+                this.onFieldChoose(choice, this.nameToTopic(option), 1)
               }
               size={this.props.size}
             />
           )}
-          {this.state.topicsBeingShown[choiceNumber] >= 1 && (
+          {this.state.topicsShown[choice] > 1 && (
             <Select
               options={this.getChildren(
-                this.state.topics[choiceNumber][0]
+                this.state.topics[choice][0]
               ).concat("All")}
               onChange={({ option }) =>
-                this.onFieldChoose(choiceNumber, this.nameToTopic(option), 1)
+                this.onFieldChoose(choice, this.nameToTopic(option), 2)
               }
               size={this.props.size}
             />
           )}
-          {this.state.topicsBeingShown[choiceNumber] >= 2 && (
+          {this.state.topicsShown[choice] > 2 && (
             <Select
               options={this.getChildren(
-                this.state.topics[choiceNumber][1]
+                this.state.topics[choice][1]
               ).concat("All")}
               onChange={({ option }) =>
-                this.onFieldChoose(choiceNumber, this.nameToTopic(option), 2)
+                this.onFieldChoose(choice, this.nameToTopic(option), 2)
               }
               size={this.props.size}
             />
           )}
-          {this.state.topicsBeingShown[choiceNumber] >= 0 && (
+          {this.state.topicsShown[choice] > 0 && (
             <Box margin={{left: "10px"}}>
-            <Close onClick={this.onCancelClick(choiceNumber)} />
+            <Close onClick={this.onCancelTopicShown(choice)} />
             </Box>
           )}
         </Box>
       );
-    } else if (choiceNumber == this.state.numberTopicsChosen + 1) {
+    } else  {
       return (
         <Box
           focusIndicator={false}
           background="white"
           round="xsmall"
           pad={{ vertical: "2px", horizontal: "xsmall" }}
-          onClick={this.onClickAddTopic}
+          onClick={this.onAddTopicShown(choice)}
           style={{
             width: "10%",
             border: "1px solid #C2C2C2",
           }}
           hoverIndicator={true}
-          align="center"   
+          align="center"
+          margin={{bottom: "24px"}}   
         >
-         
           <Text color="grey" size="small"> 
             + Add 
           </Text>
-
-          
         </Box>
       );
     }
@@ -210,7 +204,7 @@ export default class TopicSelector extends Component<Props, State> {
 
   render() {
     return (
-      <Box width="100%" direction="column">
+      <Box width="100%" direction="column" margin={{top: "12px"}}>
         {this.renderTopicChoice(0)}
         {this.renderTopicChoice(1)}
         {this.renderTopicChoice(2)}
