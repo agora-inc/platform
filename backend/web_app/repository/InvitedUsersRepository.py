4e4419450784d17f0fe4a0aeb315cd91ca3533ec
remymess <remy.messadene@gmail.com>
2020-09-25 02:28:48 +0200

    Testing endpoint and work from start to finish; remains to do: fix bug that capture return to line when entering line

--- backend/web_app/repository/InvitedUsersRepository.py
+++ backend/web_app/repository/InvitedUsersRepository.py
@@ -2,6 +2,7 @@ import random
 import logging
 from repository.ChannelRepository import ChannelRepository
 from flask_mail import Message
+from flask import render_template
 
 
 class InvitedUsersRepository:
@@ -41,13 +42,12 @@ class InvitedUsersRepository:
             '''
         return self.db.run_query(add_member_query, delete_email_mailing_list_query)
 
-    def addInvitedMemberToChannel(self, emailList: list, channelId, role):
-        #
-        # TODO: TO TEST
-        #
+    def addInvitedMemberToChannel(self, emailList, channelId, role):
         # Remove all duplicates in the email_list
-        assert(isinstance(email_list, list))
-        email_list_cleaned = list(set([email.lower() for email in email_list]))
+        if isinstance(emailList, str):
+            emailList = [emailList]
+
+        email_list_cleaned = list(set([email.lower() for email in emailList]))
 
         # A. if one of the email is already associated to a user, remove it
         registered_users_email_query = f'''
@@ -59,7 +59,6 @@ class InvitedUsersRepository:
             WHERE email IS NOT NULL
             ;
         '''
-
         registered_users_emails = self.db.run_query(registered_users_email_query)
 
         for email in email_list_cleaned:
@@ -67,29 +66,32 @@ class InvitedUsersRepository:
                 email_list_cleaned.remove(email) 
 
         # B. Insert emails in DB
-        email_list_insert = list([tuple(i[0].lower(), channelId) for i in email_list_cleaned])
-        # app.logger.debug(f"Invitation send to")
+        email_list_insert = list([tuple([i.lower(), channelId]) for i in email_list_cleaned])
+
         sql_email_list = str(email_list_insert)
         if len(sql_email_list) >=2:
             sql_email_list = sql_email_list[1:-1]
 
-        add_query = f'''INSERT INTO 'GhostUsers' (email, channel_id) VALUES {sql_email_list};'''
+        add_query = f'''INSERT INTO InvitedUsers (email, channel_id) VALUES {sql_email_list};'''
         self.db.run_query(add_query)
 
-        # send emails
+        # Send emails
         channel_name = self.channels.getChannelById(channelId)["name"]
-        text_msg = 'Agora.stream: password reset'
         for email in emailList:
-            msg = Message(text_msg, sender = 'team@agora.stream', recipients = [email])
-            msg.body = f'Hi there! This is a test from {channel_name}'
+            msg = Message(sender = 'team@agora.stream', recipients = [email])
+            msg.html = f'''
+                <p>yoyo there!</p>
+                <p>This is a <b>mega test</b>  from {channel_name}</p>
+                <p>Signe: PAtrick</p>
+                '''
             msg.subject = f"Agora.stream: invitation to {channel_name}"
             self.mail_sys.send(msg)
 
+
     def getInvitedMembersEmails(self, channelId):
-        #
-        # TODO: TO TEST
-        #
-        all_invited_emails_query = f'''SELECT email FROM InvitedMembers 
+        all_invited_emails_query = f'''SELECT email FROM InvitedUsers
             WHERE channel_id = {channelId};
             '''
-        return self.db.run_query(all_invited_emails_query)
+
+        emails = self.db.run_query(all_invited_emails_query)
+        return [i["email"] for i in emails]
