cde0f680014d7d157773e5206a3b25e09b48a1eb
Aidan Carling <55260157+AidanCarling@users.noreply.github.com>
2020-11-29 15:03:40 +0000

    2 columns instead of 3

--- frontend/src/Views/AllAgorasPage.tsx
+++ frontend/src/Views/AllAgorasPage.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Identicon } from "react-identicons";
 import { Channel, ChannelService } from "../Services/ChannelService";
 import "../Styles/all-agoras-page.css";
+import { Box } from "grommet";
 
 const AllAgorasPage = () => {
   const [agoras, setAgoras] = useState<Channel[]>([]);
@@ -14,32 +15,38 @@ const AllAgorasPage = () => {
   return (
     <div className="all-agoras-page">
       <span className="all-agoras-title">All agoras</span>
-      <div className="all-agoras-grid">
+      <div className="all-agoras-grid"
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr',
+          alignSelf: 'center'
+        }}
+      >
         {agoras.map((agora) => (
-          <Link className="agora-card" to={`/${agora.name}`}>
-            <div
-              className="agora-card-banner"
-              style={{ background: agora.colour }}
-            >
-              {
-                <img
-                  src={ChannelService.getCover(agora.id, Math.floor(new Date().getTime() / 45000))}
-                  width={420}
-                  height={140}
-                />
-              }
-            </div>
-            <div className="avatar-and-name">
-              <div className="agora-card-avatar">
-                <img
-                  src={ChannelService.getAvatar(agora.id, Math.floor(new Date().getTime() / 45000))}
-                  height={30}
-                  width={30}
-                />
+            <Link className="agora-card" to={`/${agora.name}`}>
+              <div
+                className="agora-card-banner"
+                style={{ background: agora.colour }}
+              >
+                {
+                  <img
+                    src={ChannelService.getCover(agora.id, Math.floor(new Date().getTime() / 45000))}
+                    width={420}
+                    height={140}
+                  />
+                }
               </div>
-              <span className="agora-card-name">{agora.name}</span>
-            </div>
-          </Link>
+              <div className="avatar-and-name">
+                <div className="agora-card-avatar">
+                  <img
+                    src={ChannelService.getAvatar(agora.id, Math.floor(new Date().getTime() / 45000))}
+                    height={30}
+                    width={30}
+                  />
+                </div>
+                <span className="agora-card-name">{agora.name}</span>
+              </div>
+            </Link>
         ))}
       </div>
     </div>
