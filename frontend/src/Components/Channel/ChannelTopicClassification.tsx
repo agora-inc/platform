3937060782ae8f6feb35a4c6d7bb508ea32e70e5
Aidan Carling <55260157+AidanCarling@users.noreply.github.com>
2020-12-20 16:34:09 +0000

    Adding filter to all agora page

---
+++ frontend/src/Components/Channel/ChannelTopicClassification.tsx
@@ -0,0 +1,121 @@
+import React, { Component } from "react";
+import { Box, Select, Text } from "grommet";
+import { Topic, TopicService } from "../../Services/TopicService";
+// import allTopics from "../../assets/allTopics.json"
+import "../../Styles/topic-classification.css";
+
+
+interface Props {
+  topicCallback: any
+}
+
+interface State {
+  data: Topic[],
+  topics: Topic[]
+  topicBeingShown: number;
+}
+
+export default class ChannelTopicClassification extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      data: [],
+      topics: [],
+      topicBeingShown: 0,
+    };
+  }
+
+  componentWillMount() {
+    TopicService.getAll((allTopics: Topic[]) => {
+      this.setState({ data: allTopics });
+    });
+  }
+
+  onFieldChoose = (topic: Topic, fieldDepth: number) => {
+    let temp = fieldDepth + (topic.id >= 0 ? 1 : 0)
+    let tempTopics = this.state.topics;
+    tempTopics = tempTopics.slice(0, fieldDepth);
+    tempTopics.push(topic);
+    if (topic.id === -1 && tempTopics.length >= 2) {
+      this.props.topicCallback(tempTopics[tempTopics.length - 2])
+    } else {
+      this.props.topicCallback(topic);
+    }
+    this.setState({
+      topics: tempTopics,
+      topicBeingShown: temp,
+    });
+
+    
+  };
+
+  nameToTopic = (name: string): Topic => {
+    if (name == "All") {
+      return {field: "All",
+              id: -1,
+              is_primitive_node: false,
+              parent_1_id: -1,
+              parent_2_id: -1, 
+              parent_3_id: -1}
+    } else {
+      return this.state.data
+        .filter(function (topic: Topic) {
+          return topic.field === name;
+        })
+        .map((topic: Topic) => topic)[0];
+    }
+  };
+
+  getPrimitiveNodes = () => {
+    return this.state.data
+      .filter(function (topic: any) {
+        return topic.is_primitive_node;
+      })
+      .map((topic: any) => topic.field);
+  };
+
+  getChildren = (topic: Topic) => {
+    return this.state.data
+      .filter(function (temp: Topic) {
+        return (
+          temp.parent_1_id == topic.id ||
+          temp.parent_2_id == topic.id ||
+          temp.parent_3_id == topic.id
+        );
+      })
+      .map((temp: Topic) => temp.field);
+  };
+
+  render() {
+    return (
+      <Box width="100%" direction="column">
+        <Box
+          width="100%"
+          direction="row"
+          gap="xsmall"
+          align="end"
+          margin={{ bottom: "15px" }}
+        >
+          <Text size="14px" color="grey" margin="5px" weight="bold">
+            Filter by topics
+          </Text>
+        </Box>
+
+        <div 
+          className="classification_box"
+        >
+        {this.state.topicBeingShown >= 0 && (
+          <Select
+            options={this.getPrimitiveNodes().concat("All")}
+            placeholder={"Field"}
+            dropHeight="medium"
+            onChange={({ option }) =>
+              this.onFieldChoose(this.nameToTopic(option), 0)
+            }
+          />
+        )}
+        </div>  
+      </Box>
+    );
+  };
+}
--- frontend/src/Views/AllAgorasPage.tsx
+++ frontend/src/Views/AllAgorasPage.tsx
@@ -1,10 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,Component } from "react";
+import { Box, Button, Text } from "grommet";
 import { Link } from "react-router-dom";
 import { Identicon } from "react-identicons";
 import { Channel, ChannelService } from "../Services/ChannelService";
+import { User } from "../Services/UserService";
+import { Talk, TalkService } from "../Services/TalkService";
+import { Topic, TopicService } from "../Services/TopicService";
+import ChannelTopicClassification from "../Components/Channel/ChannelTopicClassification";
+import MediaQuery from "react-responsive";
 import "../Styles/all-agoras-page.css";
 
-const AllAgorasPage = () => {
+/*const AllAgorasPage = () => {
   const [agoras, setAgoras] = useState<Channel[]>([]);
 
   useEffect(() => {
@@ -46,4 +52,159 @@ const AllAgorasPage = () => {
   );
 };
 
-export default AllAgorasPage;
+export default AllAgorasPage;*/
+
+interface Props {
+  gridArea?: string;
+  onSave?: any;
+  onUnsave?: any;
+  seeMore: boolean;
+  title: boolean;
+  topicSearch: boolean;
+  user: User | null;
+}
+
+interface State {
+  allAgoras: Channel[];
+  chosenAgoras: Channel[];
+  allTopics: Topic[];
+  chosenTopic: Topic;
+}
+
+export default class AllAgorasPage extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      allAgoras: [],
+      chosenAgoras: [],
+      allTopics: [],
+      chosenTopic: {
+        field: "-",
+        id: -1,
+        is_primitive_node: false,
+        parent_1_id: -1,
+        parent_2_id: -1,
+        parent_3_id: -1,
+      },
+    };
+  }
+
+  componentWillMount() {
+    if (this.props.topicSearch) {
+      TopicService.getAll((allTopics: Topic[]) => {
+        this.setState({ allTopics: allTopics });
+      });
+    }
+
+    // Limit to 1000 talks
+    /*
+    TalkService.getAllFutureTalks(1000, 0, (allTalks: Talk[]) => {
+      this.setState({
+        allTalks: allTalks,
+        chosenTalks: allTalks,
+      });
+    });
+    */
+    
+   ChannelService.getAllChannels(
+      100, 
+      0, 
+      (allAgoras: Channel[]) => {
+      this.setState({
+        allAgoras: allAgoras,
+        chosenAgoras: allAgoras,
+      });
+    });
+  }
+
+  selectTopic = (temp: Topic) => {
+    this.setState({
+      chosenTopic: temp,
+    });
+  };
+
+  ifTalks = () => {
+    
+    return (
+      <div className="talk_cards_outer_box">
+        {/* <Box 
+        width="100%"
+        gap="small"
+        direction="row"
+        height="100%"
+        wrap
+        margin={{ top: "24px" }}
+        > */}
+        {this.state.chosenAgoras.map((agora: Channel) => (
+            <Link className="agora-card" to={`/${agora.name}`}>
+            <div
+              className="agora-card-banner"
+              style={{ background: agora.colour }}
+            >
+              {
+                <img
+                  src={ChannelService.getCover(agora.id, Math.floor(new Date().getTime() / 45000))}
+                  width={420}
+                  height={140}
+                />
+              }
+            </div>
+            <div className="avatar-and-name">
+              <div className="agora-card-avatar">
+                <img
+                  src={ChannelService.getAvatar(agora.id, Math.floor(new Date().getTime() / 45000))}
+                  height={30}
+                  width={30}
+                />
+              </div>
+              <span className="agora-card-name">{agora.name}</span>
+            </div>
+          </Link>
+        ))}
+      </div>
+    );
+  };
+
+  ifNoTalks = () => {
+    return (
+      <Box
+        direction="row"
+        width="100%"
+        margin="none"
+        pad="small"
+        justify="between"
+        round="xsmall"
+        align="center"
+        alignSelf="center"
+        background="#F3EACE"
+      >
+        <Text size="18px" weight="bold" color="grey">
+          There are no Agoras in that category
+        </Text>
+      </Box>
+    );
+  };
+
+  render() {
+
+    return (
+      <div className="all-agoras-page">
+        <span className="all-agoras-title">Agoras</span>
+        <div className="all-agoras-grid"></div>
+        
+        
+          <ChannelTopicClassification 
+            topicCallback={this.selectTopic} 
+            />
+        
+        
+
+        {this.state.chosenAgoras.length === 0
+          
+          
+          ? this.ifNoTalks()
+          : this.ifTalks()}
+      </div>
+    );
+  }
+}
