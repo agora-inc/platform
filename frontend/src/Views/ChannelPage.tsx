7aa6fab2587a57f1d7e8f2a322be7b75c0ad6477
Aidan Carling <55260157+AidanCarling@users.noreply.github.com>
2021-01-09 22:28:19 +0000

    Adding share social media buttons to channel page

--- frontend/src/Views/ChannelPage.tsx
+++ frontend/src/Views/ChannelPage.tsx
@@ -20,6 +20,7 @@ import { CSSProperties } from "styled-components";
 import { FormDown, FormUp } from "grommet-icons";
 import ApplyToTalkForm from "../Components/Talks/ApplyToTalkForm";
 import RequestMembershipButton from "../Components/Channel/ApplyMembershipButton";
+import ChannelShareButtons from ".././Components/Channel/ChannelShareButtons";
 
 interface Props {
   location: { pathname: string };
@@ -377,7 +378,12 @@ export default class ChannelPage extends Component<Props, State> {
               <Text size="26px" color="black" weight="bold">
                 {this.state.channel ?.name}
               </Text>
-              <Box height="36px"> </Box>
+              <span style={{marginBottom: "6px", textSizeAdjust:"14px"}}>Share this Agora:</span>
+              <Box height="36px" style={{width: "300px"}}> 
+                  <ChannelShareButtons
+                    channel={this.state.channel!}
+                  />
+              </Box>
               {/*<Text size="24px" color="#999999" weight="bold">
                 {this.state.followerCount} followers
                 </Text>*/}
