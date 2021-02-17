6461957d462f019f4e858659691329e8c393594d
AlainRossier <alain6rossier@gmail.com>
2021-02-17 16:10:38 +0000

    WIP landing page

--- frontend/src/Views/LandingPage.tsx
+++ frontend/src/Views/LandingPage.tsx
@@ -4,6 +4,7 @@ import { Box, Text, Heading } from "grommet";
 import { ReactComponent as Logo } from "../apollo.svg";
 import { User, UserService } from "../Services/UserService";
 import UserManager from "../Components/Account/UserManager";
+import FooterComponent from "../Components/Homepage/FooterComponent";
 import MediaQuery from "react-responsive";
 
 interface State {
@@ -36,62 +37,155 @@ export default class LandingPage extends Component<RouteComponentProps, State> {
         direction="column"
         align="center"
       >
-        <Box direction="row" margin={{top: "50px"}}>
-          <Logo style={{ height: "60px", width: "60px"}} />
-          <Heading
-            level="1"
-            margin={{ top: "13px" }}
-            style={{ fontSize: 48, color: "black" }}
-          >
-            gora.stream
-          </Heading>
-          <Box
-            direction="row"
-            gap="small"
-            align="end"
-            style={{ minWidth: "30%", maxHeight: "20px" }}
-            justify="end"
-          >
-            <UserManager showLogin={this.state.showLogin} />
-          </Box>
+        <Box
+          direction="row"
+          gap="small"
+          align="end"
+          style={{ minWidth: "90%", maxHeight: "40px" }}
+          margin={{top: "20px"}}
+          justify="end"
+        >
+          <UserManager showLogin={this.state.showLogin} />
         </Box>
 
-        <Box direction="row"> 
-          <Text size="24px"> 
-            Learn from the best academics in the world -- live and for FREE
-          </Text>
-        </Box>
+        <Box direction="row" gap="50px">
 
-
-        <Box gap="xsmall" pad={{ vertical: "medium" }} focusIndicator={false}>
-          <Link
-            to={{ pathname: "/browse" }}
-            style={{ textDecoration: "none" }}
-          >
-            <Box
-              // onClick={this.toggleDropdown}
-              background="#7E1115"
-              round="xsmall"
-              margin={{ horizontal: "small" }}
-              pad="xsmall"
-              height="40px"
-              justify="center"
-              align="center"
-              focusIndicator={false}
-              hoverIndicator="#5A0C0F"
-            >
-              <Text size="14px"> Find future seminars </Text>
+          <Box direction="column" justify="center" margin={{right: "60px"}} >
+            <Box direction="row" margin={{top: "70px"}}>
+              <Logo style={{ height: "60px", width: "60px"}} />
+              <Heading
+                level="1"
+                margin={{ top: "13px" }}
+                style={{ fontSize: "48px", color: "black" }}
+              >
+                gora.stream
+              </Heading>
             </Box>
-          </Link>
+
+            <Box direction="column" margin={{top: "-20px"}}> 
+              <Text size="18px"> 
+                Live stream seminars from the best
+              </Text>
+              <Text size="18px"> 
+                academics in the world - for FREE!
+              </Text>
+            </Box>
+          </Box>
+          <Box 
+            direction="column" 
+            gap="50px" 
+            focusIndicator={false}
+            margin={{top: "80px"}}
+          >
+            <Link
+              to={{ pathname: "/browse" }}
+              style={{ textDecoration: "none" }}
+            >
+              <Box
+                background="#F2F2F2"
+                round="xsmall"
+                margin={{ horizontal: "small" }}
+                pad="xsmall"
+                height="100px"
+                width="162px"
+                justify="center"
+                align="center"
+                focusIndicator={false}
+                hoverIndicator="#5A0C0F"
+              >
+                <Text size="18px" weight="bold"> Find </Text>
+                <Text size="18px" weight="bold"> future seminars </Text>
+              </Box>
+            </Link>
+            <Link
+              to={{ pathname: "/past" }}
+              style={{ textDecoration: "none" }}
+            >
+              <Box
+                background="#F2F2F2"
+                round="xsmall"
+                margin={{ horizontal: "small" }}
+                pad="xsmall"
+                height="100px"
+                width="162px"
+                justify="center"
+                align="center"
+                focusIndicator={false}
+                hoverIndicator={true}
+              >
+                <Text size="18px" weight="bold"> Watch  </Text>
+                <Text size="18px" weight="bold"> past seminars </Text>
+              </Box>
+            </Link>
+          </Box>
+
+          <Box 
+            direction="column" 
+            gap="50px"
+            focusIndicator={false}
+            margin={{top: "80px"}}
+          >
+            <Link
+              to={{ pathname: "/browse" }}
+              style={{ textDecoration: "none" }}
+            >
+              <Box
+                background="#F2F2F2"
+                round="xsmall"
+                margin={{ horizontal: "small" }}
+                pad="xsmall"
+                height="100px"
+                width="162px"
+                justify="center"
+                align="center"
+                focusIndicator={false}
+                hoverIndicator="#5A0C0F"
+              >
+                <Text size="18px" weight="bold"> Create </Text>
+                <Text size="18px" weight="bold"> an Agora </Text>
+              </Box>
+            </Link>
+            <Link
+              to={{ pathname: "/agoras" }}
+              style={{ textDecoration: "none" }}
+            >
+              <Box
+                background="#F2F2F2"
+                round="xsmall"
+                margin={{ horizontal: "small" }}
+                pad="xsmall"
+                height="100px"
+                width="162px"
+                justify="center"
+                align="center"
+                focusIndicator={false}
+                hoverIndicator="#5A0C0F"
+              >
+                <Text size="18px" weight="bold"> Discover</Text>
+                <Text size="18px" weight="bold"> all Agoras </Text>
+              </Box>
+            </Link>
+          </Box>
         </Box>
 
-        <Box direction="row">
-          <Box direction="column">
-            What is an agora?
+        <Box 
+          direction="row" 
+          gap="200px"
+          margin={{top: "150px", left:"-240px"}}
+          // background="rgba(96, 110, 235, 0.7)"
+        >
+          <Box direction="column" justify="start" style={{minWidth: "50%"}}>
+            <Text size="21px"> What do we believe in? </Text>
+            <Text size="14px" margin={{top: "10px"}}> Values </Text>
           </Box>
-          <Box direction="column">
-            What do we believe in? 
+          <Box direction="column" justify="start" style={{minWidth: "50%"}}>
+            <Text size="21px"> What is an agora? </Text>
+            <Text size="14px" margin={{top: "10px"}}> An agora is like a youtube channel </Text>
           </Box>
+        </Box>
+
+        <Box width="870px">
+          <FooterComponent />
         </Box>
 
       </Box>
