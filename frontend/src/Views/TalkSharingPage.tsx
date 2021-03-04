0150d2a0aca125faab03b8d166dfb93be01e1758
AlainRossier <alain6rossier@gmail.com>
2021-03-04 00:30:35 +0000

    rework talk sharing page

--- frontend/src/Views/TalkSharingPage.tsx
+++ frontend/src/Views/TalkSharingPage.tsx
@@ -3,28 +3,10 @@ import { Redirect } from "react-router-dom";
 import { Box, Text, Image } from "grommet";
 import { User, UserService } from "../Services/UserService";
 import { Channel, ChannelService } from "../Services/ChannelService";
-import { Video, VideoService } from "../Services/VideoService";
-import { Stream, StreamService } from "../Services/StreamService";
 import { Talk, TalkService } from "../Services/TalkService";
 import { Link } from "react-router-dom";
-import Identicon from "react-identicons";
-import Loading from "../Components/Core/Loading";
-import ChannelPageTalkList from "../Components/Channel/ChannelPageTalkList";
-import ChannelPageTalkCard from "../Components/Channel/ChannelPageTalkCard";
-import VideoCard from "../Components/Streaming/VideoCard";
-import ChannelLiveNowCard from "../Components/Channel/ChannelLiveNowCard";
 import "../Styles/channel-page.css";
-import PastTalkCard from "../Components/Talks/PastTalkCard";
-import AboutUs from "../Components/Channel/AboutUs";
-import { baseApiUrl } from "../config";
 import { Calendar, Workshop, UserExpert } from "grommet-icons";
-import { CSSProperties } from "styled-components";
-import { FormDown, FormUp } from "grommet-icons";
-import ApplyToTalkForm from "../Components/Talks/ApplyToTalkForm";
-import RequestMembershipButton from "../Components/Channel/ApplyMembershipButton";
-import TalkCard from "../Components/Talks/TalkCard";
-import LoginModal from "../Components/Account/LoginModal";
-import SignUpButton from "../Components/Account/SignUpButton";
 import Countdown from "../Components/Talks/Countdown";
 import FooterOverlay from "../Components/Talks/Talkcard/FooterOverlay";
 import TalkRegistrationButton from "../Components/Talks/Talkcard/TalkRegistrationButton";
@@ -40,25 +22,9 @@ interface State {
   channel: Channel | null;
   talk: Talk;
   role: "none" | "owner" | "member" | "follower";
-  loading: boolean;
-  streams: Stream[];
-  talks: Talk[];
-  currentTalks: Talk[];
-  pastTalks: Talk[];
-  totalNumberOfpastTalks: number;
-  followerCount: number;
-  following: boolean;
   user: User | null;
-  bannerExtended: boolean;
+  registered: boolean;
   showTalkId: number;
-  membershipApplicatedFetched: boolean;
-  membershipApplication: {
-    fullName: string,
-    position: string,
-    institution: string,
-    email: string,
-    personalHomepage: string
-  }
 }
 
 export default class ChannelPage extends Component<Props, State> {
@@ -67,8 +33,6 @@ export default class ChannelPage extends Component<Props, State> {
     this.state = {
       channel: null,
       role: "none",
-      loading: true,
-      streams: [],
       talk: {
         id: NaN,
         channel_id: NaN,
@@ -91,31 +55,19 @@ export default class ChannelPage extends Component<Props, State> {
         published: 0,
         audience_level: "All"
       },
-      
-      talks: [],
-      currentTalks: [],
-      pastTalks: [],
-      totalNumberOfpastTalks: 0,
-      followerCount: 0,  // NOTE: WE STOPPED USING FOLLOWERS ATM
-      following: false,
       user: UserService.getCurrentUser(),
-      bannerExtended: true,
+      registered: false,
       showTalkId: this.getTalkIdFromUrl(),
-      membershipApplicatedFetched: false,
-      membershipApplication:
-      {
-        fullName: "",
-        position: "",
-        institution: "",
-        email: "",
-        personalHomepage: ""
-      }
     };
     this.fetchTalk();
+    this.checkIfRegistered();
+    this.fetchRoleInChannel();
   }
 
   componentWillMount() {
     this.fetchTalk();
+    this.checkIfRegistered();
+    this.fetchRoleInChannel();
   }
 
   getTalkIdFromUrl = (): number => {
@@ -126,9 +78,31 @@ export default class ChannelPage extends Component<Props, State> {
     return Number(talkId);
   };
 
-  shouldRedirect = (): boolean => {
-    return this.state.role === "owner";
-  };
+  fetchAll = () => {
+    let talkId = this.getTalkIdFromUrl();
+    let channelId = 0; // this.getChannelIdFromTalkId();
+    TalkService.getTalkById(talkId, (talk: Talk) => {
+        this.setState({talk: talk})
+    });
+
+    this.state.user && 
+      ChannelService.getRoleInChannel(
+        this.state.user.id, 
+        channelId, 
+        (role: "none" | "owner" | "member" | "follower") => {
+          this.setState({role: role})
+      })
+
+    this.state.user &&
+      TalkService.isRegisteredForTalk(
+        talkId,
+        this.state.user.id,
+        (registered: any) => {
+          this.setState({ registered });
+        }
+      );
+
+  }
 
   fetchTalk = () => {
     let talkId = this.getTalkIdFromUrl();
@@ -137,106 +111,25 @@ export default class ChannelPage extends Component<Props, State> {
     });
   }
 
-
-
-  
-  checkIfFollowing = () => {
-    ChannelService.getRoleInChannel(
-      this.state.user!.id,
-      this.state.channel!.id,
-      (role: string) => {
-        this.setState({ following: role === "follower" });
-      }
-    );
-  };
-
-  checkIfMembershipRequested = () => {
-    // If user not logged in, put a log in/ register inside the box
-
-
-    // else: check the application and fill the fields with the past application
-    if (!(this.state.membershipApplicatedFetched)) {
-      ChannelService.getMembershipApplications(
-        this.state.channel!.id,
-        (
-          full_name: string,
-          position: string,
-          institution: string,
-          email: string,
-          personalHomepage: string
-        ) => {
-          this.setState({
-            membershipApplication: {
-              fullName: full_name,
-              position: position,
-              institution: institution,
-              email: email,
-              personalHomepage: personalHomepage
-            }
-          })
-        },
-      this.state.user!.id,
-      )
-    };
-    }
-
-
-
-  onApplyMembershipClicked = () => {
-  };
-
-  onFollowClicked = () => {
-    // NOTE: DISABLED
-    if (!this.state.following) {
-      ChannelService.addUserToChannel(
-        this.state.user!.id,
-        this.state.channel!.id,
-        "follower",
-        () => {
-        }
-      );
-      this.setState({ role: "follower" });
-    } else {
-      ChannelService.removeUserFromChannel(
-        this.state.user!.id,
-        this.state.channel!.id,
-        () => {
-        }
-      );
-      if (!(this.state.role === "member")){
-        this.setState({ role: "none" });
-      }
-    }
-    this.setState({ following: !this.state.following });
-  };
-
-  getImageUrl = (): string | undefined => {
-    let current_time = Math.floor(new Date().getTime() / 5000);
-    let imageUrl = this.state.channel?.id
-      ? `${baseApiUrl}/channels/cover?channelId=${this.state.channel.id}&ts=` + current_time
-      // HACK: we add the new time at the end of the URL to avoid caching; 
-      // we divide time by value such that all block of requested image have 
-      // the same name (important for the name to be the same for the styling).
-      : undefined;
-    return imageUrl;
+  fetchRoleInChannel = () => {
+    this.state.user && 
+      ChannelService.getRoleInChannel(
+        this.state.user.id, 
+        this.state.talk.channel_id, 
+        (role: "none" | "owner" | "member" | "follower") => {
+          this.setState({role: role})
+      })
   }
 
-  getCoverBoxStyle = (): CSSProperties => {
-    let background = this.state.channel ?.colour;
-    let border = "none";
-
-    return {
-      width: "75vw",
-      borderTopRightRadius: 10,
-      borderTopLeftRadius: 10,
-      background: background,
-      backgroundSize: "75vw 25vw",
-      border: border,
-    };
-  };
-
-  toggleBanner = () => {
-    this.setState({ bannerExtended: !this.state.bannerExtended });
+  checkIfRegistered = () => {
+    this.state.user &&
+      TalkService.isRegisteredForTalk(
+        this.state.talk.id,
+        this.state.user.id,
+        (registered: any) => {
+          this.setState({ registered });
+        }
+      );
   };
 
   formatDateFull = (s: string, e: string) => {
@@ -249,129 +142,12 @@ export default class ChannelPage extends Component<Props, State> {
     return `${dateStartStr} ${timeStartStr} - ${timeEndStr} `;
   };
 
-  banner = () => {
-    return (
-      <Box width="75vw" background="white" round="10px">
-        <Box
-          direction="row"
-          justify="between"
-          height="25vw"
-        >
-          <Image src={this.getImageUrl()} style={this.getCoverBoxStyle()} />
-        </Box>
-        <Box
-          direction="row"
-          height="133px"
-          align="center"
-          justify="between"
-          pad="16px"
-        >
-          <Box direction="row" align="end" gap="small">
-            <Box
-              width="100px"
-              height="100px"
-              round="50px"
-              background="white"
-              justify="center"
-              align="center"
-              style={{ minWidth: 100, minHeight: 100 }}
-              overflow="hidden"
-            >
-              {(
-                <img
-                  src={
-                    ChannelService.getAvatar(this.state.channel!.id, Math.floor(new Date().getTime() / 45000))
-                  }
-                  // HACK: we had the ts argument to prevent from caching.
-                  height={100}
-                  width={100}
-                />
-              )}
-            </Box>
-            <Box>
-              <Text size="30px" color="black" weight="bold">
-                {this.state.channel ?.name}
-              </Text>
-              <Box height="36px"> </Box>
-              {/*<Text size="24px" color="#999999" weight="bold">
-                {this.state.followerCount} followers
-                </Text>*/}
-            </Box>
-          </Box>
-          <Box direction="row" gap="xsmall" align="center">
-
-            {!(this.state.role == "member" || this.state.role == "owner") && (
-            <RequestMembershipButton
-              channelId={this.state.channel!.id}
-              channelName={this.state.channel!.name}
-              user={this.state.user}
-            />
-            )}
-
-            {this.state.user && (
-              <Box
-                className="follow-button"
-                background={this.state.following ? "#e5e5e5" : "white"}
-                height="45px"
-                style={{
-                  border: "1px solid #C2C2C2",
-                }}
-                width="10vw"
-                round="xsmall"
-                pad={{bottom: "6px", top: "6px", left: "18px", right: "18px"}}
-                align="center"
-                justify="center"
-                onClick={this.onFollowClicked}
-                focusIndicator={false}
-                hoverIndicator={true}
-              >
-                <Text 
-                  size="16px" 
-                  color="grey"
-                  alignSelf="center"
-                >
-                  {this.state.following ? "Following" : "Follow"}
-                </Text>
-              </Box>
-            )}
-            {this.state.bannerExtended ? (
-              <FormUp
-                onClick={this.toggleBanner}
-                size="50px"
-                color="black"
-                style={{ cursor: "pointer" }}
-              />
-            ) : (
-                <FormDown
-                  onClick={this.toggleBanner}
-                  size="50px"
-                  color="black"
-                  style={{ cursor: "pointer" }}
-                />
-              )}
-          </Box>
-        </Box>
-        {this.state.bannerExtended && (
-          <Text
-            size="20px"
-            style={{ textAlign: "justify", fontWeight: 450 }}
-            margin={{ horizontal: "16px", bottom: "16px" }}
-          >
-            <div
-              dangerouslySetInnerHTML={{
-                __html: this.state.channel ?.long_description
-                  ? this.state.channel ?.long_description
-                    : "",
-              }}
-            />
-          </Text>
-        )}
-      </Box>
-    );
-  };
-
   render() { 
     const talk = this.state.talk;
+    console.log("talk", talk)
+    console.log("role", this.state.role)
+    console.log("registered", this.state.registered)
+
       return(
         <>
         <Helmet>
@@ -535,13 +311,13 @@ export default class ChannelPage extends Component<Props, State> {
                 user={this.props.user}
               /> */}
               
-              {((!this.state.user || talk.visibility !== "Members only") && (talk.visibility !== "Everybody")) && ( 
+              {!(this.state.role in ["owner", "member"]) && !this.state.registered && talk.visibility !== "Everybody" && ( 
                 <TalkRegistrationButton
                   talk={talk}
                   user={this.state.user}
                 />
               )}
-              {((this.state.user && talk.visibility === "Members only") || (talk.visibility === "Everybody")) && ( 
+              {(this.state.role in ["owner", "member"] || this.state.registered || talk.visibility === "Everybody") && ( 
                 <Countdown talk={talk} />
               )}
             </Box>
