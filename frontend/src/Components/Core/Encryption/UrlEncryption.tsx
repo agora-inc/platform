f987141b1888bbd38ca6dff90740555e29543b84
remymess <remy.messadene@gmail.com>
2021-04-12 22:50:15 +0200

    Adding encryption and decryption of URLs using AES and package crypto-ts

--- frontend/src/Components/Core/Encryption/UrlEncryption.tsx
+++ frontend/src/Components/Core/Encryption/UrlEncryption.tsx
@@ -1,16 +1,57 @@
-import React, { Component } from "react";
+var CryptoTS = require("crypto-ts");
 
-interface Props {
-  url: string;
-}
+const sep = "w3b45t5sqjl2sk452i3t5sqjl2i3w"
+const sk = "qwe#';/.,"
 
-export default class UrlEncryption extends Component<Props> {
-  constructor(props: Props) {
-    super(props);
-    this.state 
+function getRandomInt(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
   }
 
-  render() {
-      return ""
-  }
+export function encryptUrl(baseUrl: string, id: number, role?: string) {
+    var data = [{id: 1}, {id: 2}]
+
+    // Encrypt
+    // var encodedEndpoint = CryptoTS.AES.encrypt(JSON.stringify(data), 'secret key 123');
+    const encodedId = CryptoTS.AES.encrypt(id.toString(), sk).toString();
+    var encodedRole = ""
+    if (role === null){
+        role = getRandomInt(0, 1000).toString();
+    }
+    
+    encodedRole = CryptoTS.AES.encrypt(role, sk).toString()
+    
+    var encodedEndpoint = encodedId.concat(sep, encodedRole);
+
+    let lastChar = baseUrl.charAt(baseUrl.length-1);
+    if (lastChar == "/"){
+        return baseUrl + encodedEndpoint
+    } else {
+        return baseUrl + "/" + encodedEndpoint
+    }
 }
+
+export function getIdFromUrl(encryptedUrl: string) {
+    
+    const SplitUrl = encryptedUrl.split("/")
+    const encryptedEndpoint = SplitUrl[SplitUrl.length-1]
+    // const basepoint = encryptedUrl.replace(encryptedEndpoint, "")
+
+    const encryptedId = encryptedEndpoint.split(sep)[0];
+
+    var bytes  = CryptoTS.AES.decrypt(encryptedId.toString(), sk);
+    return bytes.toString(CryptoTS.enc.Utf8);
+}
+
+export function getStreamingRoleFromUrl(encryptedUrl: string) {
+    const SplitUrl = encryptedUrl.split("/")
+    const encryptedEndpoint = SplitUrl[SplitUrl.length-1]
+    // const basepoint = encryptedUrl.replace(encryptedEndpoint, "")
+
+    const encryptedRole = encryptedEndpoint.split(sep)[1]
+
+    var bytes  = CryptoTS.AES.decrypt(encryptedRole.toString(), sk);
+    return bytes.toString(CryptoTS.enc.Utf8);
+}
+
