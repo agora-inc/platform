0ee9671cb2bd05b8c5ee4448b9d16369cfc32a5e
remymess <remy.messadene@gmail.com>
2021-04-12 21:12:29 +0200

    WIP

---
+++ frontend/src/Components/Core/Encryption/UrlEncryption.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+interface Props {
+  url: string;
+}
+
+export default class UrlEncryption extends Component<Props> {
+  constructor(props: Props) {
+    super(props);
+    this.state 
+  }
+
+  render() {
+      return ""
+  }
+}
