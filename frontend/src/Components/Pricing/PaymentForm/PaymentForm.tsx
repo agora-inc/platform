9e7570992c2ae84add81bf6dd8ee77736989debb
remymess <remy.messadene@gmail.com>
2021-06-25 00:23:16 +0200

    Adding backend react.js stripe

--- frontend/src/Components/Pricing/PaymentForm/PaymentForm.tsx
+++ frontend/src/Components/Pricing/PaymentForm/PaymentForm.tsx
@@ -1,80 +1,44 @@
-import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js"
-import axios from "axios"
-import React, { useState } from 'react'
+import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
 
 
-const CARD_OPTIONS = {
-	iconStyle: "solid",
-	style: {
-		base: {
-			iconColor: "#c4f0ff",
-			color: "#fff",
-			fontWeight: 500,
-			fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
-			fontSize: "16px",
-			fontSmoothing: "antialiased",
-			":-webkit-autofill": { color: "#fce883" },
-			"::placeholder": { color: "#87bbfd" }
-		},
-		invalid: {
-			iconColor: "#ffc7ee",
-			color: "#ffc7ee"
-		}
-	}
-}
+export const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
 
-export default function PaymentForm() {
-    const [success, setSuccess ] = useState(false)
-    const stripe = useStripe()
-    const elements = useElements()
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    // Block native form submission.
+    event.preventDefault();
 
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        const {error, paymentMethod} = await stripe.createPaymentMethod({
-            type: "card",
-            card: elements.getElement(CardElement)
-        })
-
-
-    if(!error) {
-        try {
-            const {id} = paymentMethod
-            const response = await axios.post("http://localhost:4000/payment", {
-                amount: 1000,
-                id
-            })
-
-            if(response.data.success) {
-                console.log("Successful payment")
-                setSuccess(true)
-            }
-
-        } catch (error) {
-            console.log("Error", error)
-        }
-    } else {
-        console.log(error.message)
+    if (!stripe || !elements) {
+      // Stripe.js has not loaded yet. Make sure to disable
+      // form submission until Stripe.js has loaded.
+      return;
     }
-}
 
-    return (
-        <>
-        {!success ? 
-        <form onSubmit={handleSubmit}>
-            <fieldset className="FormGroup">
-                <div className="FormRow">
-                    <CardElement options={CARD_OPTIONS}/>
-                </div>
-            </fieldset>
-            <button>Pay</button>
-        </form>
-        :
-       <div>
-           <h2>You just bought a sweet spatula congrats this is the best decision of you're life</h2>
-       </div> 
-        }
-            
-        </>
-    )
-}
+    // Get a reference to a mounted CardElement. Elements knows how
+    // to find your CardElement because there can only ever be one of
+    // each type of element.
+    const cardElement = elements.getElement(CardElement);
+
+    // Use your card Element with other Stripe.js APIs
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+      type: 'card',
+      card: cardElement!,
+    });
+
+    if (error) {
+      console.log('[error]', error);
+    } else {
+      console.log('[PaymentMethod]', paymentMethod);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement />
+      <button type="submit" disabled={!stripe}>
+        Pay
+      </button>
+    </form>
+  );
+};
--- frontend/src/Components/Pricing/PaymentForm/StripeContainer.tsx
+++ frontend/src/Components/Pricing/PaymentForm/StripeContainer.tsx
@@ -2,7 +2,7 @@
 import { Elements } from "@stripe/react-stripe-js"
 import { loadStripe } from "@stripe/stripe-js"
 import React from "react"
-import PaymentForm from "./PaymentForm"
+import { PaymentForm } from "../PaymentForm/PaymentForm";
 import { stripePublicKey } from "../../../config";
 
 const stripeTestPromise = loadStripe(stripePublicKey)
