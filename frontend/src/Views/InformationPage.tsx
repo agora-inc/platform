38aad24d59b19737b025d055484ad22f9b0b87dc
AlainRossier <alain6rossier@gmail.com>
2021-08-02 12:25:44 +0100

    polish about us and pricing plans

--- frontend/src/Views/InformationPage.tsx
+++ frontend/src/Views/InformationPage.tsx
@@ -63,7 +63,7 @@ export default class InformationPage extends Component<Props, State> {
           <source src="https://video.wixstatic.com/video/9b9d14_37244669d1c749ab8d1bf8b15762c61a/720p/mp4/file.mp4" type="video/mp4" />
         </video>
 
-        <Box width={renderMobileView ? "85%" : "70%"} direction="column" margin={{top: "80px"}}>
+        <Box width={renderMobileView ? "85%" : "75%"} direction="column" margin={{top: "80px"}}>
           <Box direction="row" gap="small" margin={{bottom: "36px"}}>
             <Text size={this.state.sizeHeader} weight="bold" color="color1"> 
               Our vision 🌎 
@@ -120,11 +120,14 @@ export default class InformationPage extends Component<Props, State> {
                 <Text size="25px" weight="bold" color="color3" margin={{bottom: "20px"}}>
                   Research is a social activity
                 </Text>
-                <Text size="18px">
-                  In 1989, the World Wide Web was created by scientists to facilitate international scientific research communications. 
-                  While the original network only consisted in research groups from all around the world, 
-                  the many problems it palliated rapidly led to a massive democratisation that completely diluted the former in the mass.
-                  We want to offer an easy way to every academics to rapidly and efficiently search and connect with any other research community and their events.
+                <Text size="16px" margin={{bottom: "10px"}}>
+                  Scientists created the World Wide Web to facilitate international research communications.
+                </Text>
+                <Text size="16px" margin={{bottom: "10px"}}>
+                  The internet grew massively since 1989, losing track of its original mission in the meantime.
+                </Text>
+                <Text size="16px" margin={{bottom: "10px"}}>
+                  We recreate a safe place for academics to exchange ideas without external noise.
                 </Text>
               </Box>
 
@@ -132,9 +135,14 @@ export default class InformationPage extends Component<Props, State> {
                 <Text size="25px" weight="bold" color="color3" margin={{bottom: "20px"}}>
                   Equal chances to all academics around the world
                 </Text>
-                <Text size="18px">
-                  Research is a social activity. However, some academics can experience difficulties connecting and taking part to their community events if they are hosted far from where they are. 
-                  We believe that online streaming technologies can be augmented to palliate to this problem.
+                <Text size="16px" margin={{bottom: "10px"}}>
+                  Not every researcher have access to top-notch seminars in their university.
+                </Text>
+                <Text size="16px" margin={{bottom: "10px"}}>
+                  Ideas and collaborations are most often formed around a coffee after a presentation. 
+                </Text>
+                <Text size="16px" margin={{bottom: "10px"}}>
+                  Online streaming technologies can facilitate the rise of talented researchers.
                 </Text>
               </Box>
 
@@ -142,9 +150,14 @@ export default class InformationPage extends Component<Props, State> {
                 <Text size="25px" weight="bold" color="color3" margin={{bottom: "20px"}}>
                   Facilitate cross-subject collaborations
                 </Text>
-                <Text size="18px">
-                  A large class of ground-breaking results arises from the merging of ideas coming from different communities. However, it is not always easy for two different communities to meet and identify common problems 
-                  without an active organisational effort allowing them to be physically present at the same place. We hope to facilitate conversations and collaborations by making the organisation of seminars as easy as possible and by providing a central platform where people can easily jump from a seminar to another.
+                <Text size="16px" margin={{bottom: "10px"}}>
+                  Ground-breaking results often come from the merging of ideas from different fields.
+                </Text>
+                <Text size="16px" margin={{bottom: "10px"}}>
+                  However, in order to meet, there must be a conscious organizational effort to gather.
+                </Text>
+                <Text size="16px" margin={{bottom: "10px"}}>
+                  The solution? A centralized place where researchers hop from one seminar to another.
                 </Text>
               </Box>
 
@@ -152,17 +165,17 @@ export default class InformationPage extends Component<Props, State> {
                 <Text size="25px" weight="bold" color="color3" margin={{bottom: "20px"}}>
                   Hybrid seminars are the future of academia
                 </Text>
-                <Text size="14px" margin={{bottom: "10px"}}>
-                  The COVID-19 pandemic raised awareness that researchers could benefit from online seminars.
+                <Text size="16px" margin={{bottom: "10px"}}>
+                  COVID-19 raised awareness that researchers could benefit from online seminars.
                 </Text>
-                <Text size="14px" margin={{bottom: "10px"}}>
-                  The majority of groups that went online saw their audience multiply... so why stop? 
+                <Text size="16px" margin={{bottom: "10px"}}>
+                  The majority of groups that went online saw their audience multiply. 
                 </Text>
-                <Text size="14px" margin={{bottom: "10px"}}>
-                  But as restrictions are being lifted, we LOVE having our seminars back in person.  
+                <Text size="16px" margin={{bottom: "10px"}}>
+                  As restrictions are being lifted, academics LOVE having their seminars back in person.  
                 </Text>
-                <Text size="14px" margin={{bottom: "10px"}}>
-                  The solution? An online streaming technolgy tailored for academic seminars!
+                <Text size="16px" margin={{bottom: "10px"}}>
+                  The solution? An hybrid streaming technolgy tailored for academic seminars.
                 </Text>
               </Box>
             </Grid>
@@ -175,17 +188,17 @@ export default class InformationPage extends Component<Props, State> {
               </Text>
               <Box alignContent="start">
                 <Text size="25px" weight="bold" color="color3" margin={{bottom: "20px", left: "15px"}}>
-                We are here to make a positive difference in the world of research.
+                  We want to make a positive difference in the world of research.
                 </Text>
               </Box>
               <Box alignContent="start">
                 <Text size="25px" weight="bold" color="color3" margin={{bottom: "20px", left: "15px"}}>
-                We are bold and build products we believe in.
+                  We are bold and build products we believe in.
                 </Text>
               </Box>
               <Box alignContent="start">
                 <Text size="25px" weight="bold" color="color3" margin={{bottom: "20px", left: "15px"}}>
-                We thrive for perfection in a small set of tasks over mediocrity in a large set of them.
+                  We believe in full transparency and honest communication.
                 </Text>
               </Box>
             </Box>
@@ -229,7 +242,8 @@ export default class InformationPage extends Component<Props, State> {
             </Box>
             <Box margin={{bottom: "150px"}}>
               <Text size="18px">
-                  We are a team of researchers at Imperial College London and the University of Oxford who believe in a connected world where ideas can be exchanged without any friction among inter- and intra-disciplinary communities in an efficient way.
+                  We are a team of researchers at Imperial College London and the University of Oxford 
+                  who believe in a connected world where ideas are shared in a efficient way.
                 </Text>
             </Box>
           </Box>
