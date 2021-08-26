47e012d589f628a7afc9c246ec42f0e91a86e1b2
argoopjmc <46709364+argoopjmc@users.noreply.github.com>
2021-08-26 17:46:06 +0530

    Using Raph's rewording

--- frontend/src/Views/AgoraCreationPage.tsx
+++ frontend/src/Views/AgoraCreationPage.tsx
@@ -127,7 +127,7 @@ export default class AgoraCreationPage extends Component<Props, State> {
         >
         <Box width={renderMobileView ? "85%" : "70%"} direction="column" margin={{top: "10px"}}>
             <Text size={this.state.sizeHeader} weight="bold" color="color1" margin={{bottom: "44px"}} >
-              Enhancing the seminar experience and simplifying their organisation with cutting-edge technologies.
+            Enhancing the academic seminar experience with cutting-edge technologies.
             </Text>
             <Grid
               rows={
