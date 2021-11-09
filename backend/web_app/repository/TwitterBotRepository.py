a10368979e6b74a9926d5b6bfbf2fc3fcd95e9f8
remymess <remy.messadene@gmail.com>
2021-11-09 00:47:08 +0000

    Adding workflow Twitter; remains to do: scheduling messages to post + mass follow + mass unfollow

--- backend/web_app/repository/TwitterBotRepository.py
+++ backend/web_app/repository/TwitterBotRepository.py
@@ -1,9 +1,14 @@
 import random
 import logging
-from repository.TalkRepository import TalkRepository
 from mailing.sendgridApi import sendgridApi
 from app.databases import agora_db
 import json
+
+
+# DELETE BELOW LINE IN PRODUCTION
+# from TalkRepository import TalkRepository
+# UNCOMMENT BELOW LINE IN PRODUCTION
+# from repository.TalkRepository import TalkRepository
 
 
 MAIL_SYS = sendgridApi()
@@ -13,7 +18,7 @@ class TwitterBotRepository:
     def __init__(self, db=agora_db, mail_sys=MAIL_SYS):
         self.db = db
         self.mail_sys = mail_sys
-        self.talks = TalkRepository(db=agora_db)
+        # self.talks = TalkRepository(db=agora_db)
         
     def getIncomingTalksToTweet(self, minimum_clickcount, max_day_in_advance):
         # Check public events with number views above threshold + events that haven't been advertised yet!
@@ -27,6 +32,8 @@ class TwitterBotRepository:
                     AND Talks.date > CURRENT_TIMESTAMP
                     AND Talks.card_visibility = "Everybody"
                     AND Talks.visibility = "Everybody"
+                    AND Talks.name <> 'TBA'
+                    AND Talks.name <> 'TBD'
                     AND TalkViewCounts.total_views > {minimum_clickcount}
             ORDER BY date DESC;
             '''
@@ -34,34 +41,47 @@ class TwitterBotRepository:
 
         # filter to prevent reposting
         select_successful_tweets_id = f'''
-            SELECT talk_id FROM TwitterBot 
-            WHERE talk_id IS NOT NULL AND action = 'post' AND status = 'success';
+            SELECT DISTINCT talk_id FROM TwitterBot 
+            WHERE talk_id IS NOT NULL AND (action = 'advertise' OR action = 'remind') AND success = 1;
         '''
-        #
-        #
-        # TO TEST
-        #
-        #
-        #
-        list_ids = self.db.run_query(select_successful_tweets_id)
+        list_ids_res = self.db.run_query(select_successful_tweets_id)
+        list_ids = [i["talk_id"] for i in list_ids_res]
 
-        return [i for i in res if i["id"] not in select_successful_tweets_id]
+        if list_ids == []:
+            return talks_res
+        else:
+            talks_to_tweet = []
+            for i in talks_res:
+                if i["id"] not in list_ids:
+                    talks_to_tweet.append(i)
+            return talks_to_tweet
 
-    def updateTweetSendingStatus(self, action, talk_id, successful, error_message=""):
-       # Add
+    def updateTweetSendingStatus(self, action, talk_id, success, params={}, error_msg=""):
+        print("he1")
+        assert(action in ["advertise", "remind", "retweet", "follow"])
+        print("he2")
+        
         try:
-            insert_query = f'''
-                INSERT (action, talk_id, successful, error_message) INTO TwitterBot VALUES ('post',{talk_id},{successful},{error_message})
-            '''
+            if "exec_time" in params:
+                exec_time = params["exec_time"]
+                insert_query = f'''
+                    INSERT INTO TwitterBot (action, talk_id, success, error_msg, exec_time) VALUES ('{action}',{talk_id},{success},'{error_msg}', '{exec_time}');
+                '''
+            else:
+                insert_query = f'''
+                    INSERT INTO TwitterBot (action, talk_id, success, error_msg) VALUES ('{action}',{talk_id},{success},'{error_msg}');
+                '''
             self.db.run_query(insert_query)
         except Exception as e:
+            print("heyo", e)
             insert_query = f'''
-                INSERT (action, talk_id, successful, error_message) INTO TwitterBot VALUES ('post',{talk_id},0,{e})
-            '''
+                INSERT INTO TwitterBot (action, talk_id, success, error_msg) VALUES ('{action}',{talk_id},0,'{e}');
+                '''
+            self.db.run_query(insert_query)
 
-        # If addition successful, remove unsuccessful entries
-        if successful:
-            removal_query = f'''
-                DELETE * from TwitterBot WHERE talk_id = {talk_id} AND action = '{action}';
-            '''
-            self.db.run_query(removal_query)
+        # # If addition successful, remove past unsuccessful entries
+        # if success:
+        #     removal_query = f'''
+        #         DELETE * from TwitterBot WHERE talk_id = {talk_id} AND action = '{action}';
+        #     '''
+        #     self.db.run_query(removal_query)
--- backend/web_app/twitter_bot.py
+++ backend/web_app/twitter_bot.py
@@ -1,15 +1,18 @@
 # import hack (Remy); TODO: make import directly from repository folder.
 from app.routes import TalkRepository, authenticate
-from app.routes import TweetsRepository
+from app.routes import TwitterBotRepository
 from app.databases import agora_db
 import tweepy
 import json
 import random
 
-MINIMUM_CLICKCOUNT = 4
+MINIMUM_CLICKCOUNT = 3
 MAX_DAYS_IN_ADVANCE = 5
-TWITT_API_KEY = "bPddsEy034JlKrHHzJCSGj3lf"
-TWITT_API_KEY_SEC = "bPddsEy034JlKrHHzJCSGj3lf"
+REMINDER_MINUTES_BEFORE = 15
+
+
+TWITT_CONS_KEY = "vrlWLkldnc7vsbYLTgkBv6wHv"
+TWITT_CONS_KEY_SEC = "WbaQERpKxVBCHdKxDepVxCYVQ8dnNoz58vbY4DEbl93WG4J9v0"
 TWITT_ACCESS_TOKEN = "1367790358659272704-FvOHuZRFvwkZRXxkft02QioEDPK9LR"
 TWITT_ACCESS_TOKEN_SEC = "NFMXeCpBI7MyGJCpqtRSiUNEoZJ8rAE3LsNwvM3iVi4mv"
 
@@ -20,25 +23,28 @@ TWITTER_HASHTAGS_JSON_PATH = "/home/cloud-user/plateform/agora/backend/twitter_b
 class TwitterBot:
     def __init__(self, db=agora_db):
         self.db = db
-        self.tweets = TweetsRepository.TweetsRepository()
+        self.tweets = TwitterBotRepository.TwitterBotRepository()
         self.talks = TalkRepository.TalkRepository(db=db)
         self.hashtags_per_id = {}
 
         self._load_mapping()
-        self._authenticate_twitter()
+        self.twitter_api = self._create_api()
 
-    def _authenticate_twitter(self):
+    def _create_api(self):
         # Authenticate to Twitter
-        auth = tweepy.OAuthHandler(TWITT_API_KEY, TWITT_API_KEY_SEC)
+        auth = tweepy.OAuthHandler(TWITT_CONS_KEY, TWITT_CONS_KEY_SEC)
         auth.set_access_token(TWITT_ACCESS_TOKEN, TWITT_ACCESS_TOKEN_SEC)
 
         # Create API object
-        self.twitter_api = tweepy.API(auth)
+        api = tweepy.API(
+            auth)
+            # wait_on_rate_limit=True)
         try:
-            self.twitter_api.verify_credentials()
+            api.verify_credentials()
             print("Authentication OK")
         except Exception as e:
             raise ConnectionError(f"Error during authentication {e}")
+        return api
 
     def _load_mapping(self):
         with open(TWITTER_HASHTAGS_JSON_PATH, "r") as twitter_hashtags:
@@ -46,37 +52,57 @@ class TwitterBot:
 
     def run(self):
         # A. Post talks
-        self.post_tweets()
+        # self.post_tweets()
 
-        # B. Unfollow / Follow
-        # self.follow_in_mass()
+        # # B. Unfollow / Follow
+        self.follow_in_mass()
+        # self.mass_unfollow()
 
     def post_tweets(self):
         talks = self.tweets.getIncomingTalksToTweet(MINIMUM_CLICKCOUNT, MAX_DAYS_IN_ADVANCE)
         print(talks)
         for talk in talks:
+            talk_id = talk["id"]
+            talk_name = talk["name"]
+            channel_name = talk["channel_name"]
+            speaker_name = talk["talk_speaker"]
+            date = talk["date"]
+            talk_topics_list = []
+            for key in ["topic_1_id", "topic_2_id", "topic_3_id"]:
+                topic_id = talk[key]
+                if topic_id != 0:
+                    talk_topics_list.append(topic_id)
+            text = self.get_twitter_message(talk_id, talk_name, channel_name, speaker_name, date, talk_topics_list)
+            # 1. Advertise
             try:
-                talk_id = talk["id"]
-                talk_name = talk["talk_name"]
-                channel_name = talk["channel_name"]
-                speaker_name = talk["talk_speaker"]
-                date = talk["date"]
-                talk_topics = []
-                for key in ["topic_1_id", "topic_2_id", "topic_3_id"]:
-                    topic_id = talk[key]
-                    if topic_id != 0:
-                        talk_topics.append(topic_id)
-                text = self.get_twitter_message(talk_id, talk_name, channel_name, speaker_name, date, talk_topics)
-
                 # self.twitter_api.send(FILL THIS UP)
-                self.tweets.updateTweetSendingStatus(talk_id, successful=True)
+                # self.tweets.updateTweetSendingStatus("advertise", talk_id, True)
+                pass
+           
             except Exception as e:
-                self.tweets.updateTweetSendingStatus(talk_id, successful=False, error_message=e)
+                print(f"exception yo: {e}")
+                self.tweets.updateTweetSendingStatus("advertise", talk_id, success=False, error_message=e)
+
+            # 2. Remind N minutes before the start
+            try:
+                exec_time = talk["date"]
+                print("date") 
+                print(exec_time) 
+                # self.twitter_api.send(FILL THIS UP)
+                self.tweets.updateTweetSendingStatus("remind", talk_id, True, params={"exec_time": exec_time})
+           
+            except Exception as e:
+                print(f"exception yo: {e}")
+                self.tweets.updateTweetSendingStatus("remind", talk_id, success=False, error_message=e)
 
     def get_twitter_message(self, talk_id: int, talk_name: str, channel_name: str, speaker_name: str, date: str, topic_ids: list):
         # human_readable_date (TODO)
         event_url = f"https://mora.stream/event/{talk_id}"
-        hashtags_string = [f"#{self.hashtags_per_id[topic_id]}" for topic_id in topic_ids] + "#academicTwitter"
+        hashtags_string = " "
+        for topic_id in topic_ids:
+            if topic_id in self.hashtags_per_id:
+                hashtags_string += f"#{self.hashtags_per_id[topic_id]} "
+        hashtags_string += " #academicTwitter"
         
         message = random.choice([
             "Popular event incoming!",
@@ -90,6 +116,13 @@ class TwitterBot:
             "Wondering what"
         ])
 
+        # Check Twitter char limit.
+        #
+        #
+        #
+
+
+
         return message
 
     def _track_status_tweet(self, sent: str, error_message: str):
@@ -99,13 +132,52 @@ class TwitterBot:
         raise NotImplementedError()
 
     def follow_in_mass(self):
-        # get ids of people who dont follow us (ids_to_follow)
-        # get ids of all people hwo follow us (ids_followers)
-        # unfollow all people who follow us but a small set
-        # follow ids_to_follow - ids_followers
+        print("Retrieving and following followers")
+        for follower in tweepy.Cursor(self.twitter_api.get_followers).items():
+            print("yes,letsgo")
+            follower.follow()
+            if not follower.following:
+                print("followed ", follower.name)
 
-        # Caution: if we unfollow and refollow a few people in a row, Twitter bans us for 3 days
-        raise NotImplementedError()
+
+    def mass_unfollow(self):
+        raise NotImplementedError
+        # # hits_left = self.twitter_api.rate_limit_status()
+        # # print("yoyo")
+        # # print(hits_left)['remaining_hits']
+        # # print("You can unfollow " + str(hits_left) + " people this hour...\n")
+        # print("Checking who doesn't follow you back. This will take a minute.\n")
+        # # first, create some lists to hold the followers
+        # followers = []
+        # friends = []
+
+        # # we have to use a Cursor for pagination purposes
+        # for follower in tweepy.Cursor(self.twitter_api.followers).items():
+        #     followers.append(follower)
+
+
+        # for friend in tweepy.Cursor(self.twitter_api.friends).items():
+        #     friends.append(friend)
+
+        # # create a non_reciprocals list, these are non-followers (set - set)
+        # non_reciprocal = list(set(friends) - set(followers))
+        # print(str(len(non_reciprocal)) + " non-reciprocal followers.\n")
+
+        # # count the number of people we unfollow, just for fun
+        # counter = 0
+        # for i in non_reciprocal:
+        #     # if hits_left > 0:
+        #     self.twitter_api.destroy_friendship(i.screen_name)
+        #     print("Successfully unfollowed " + i.screen_name)
+        #     hits_left -= 1
+        #     # else:
+        #     #     print("You ran out of hits! Try again in an hour!")
+
+        #     counter += 1
+        #     print("You unfollowed " + str(counter) + " people!\n")
+        #     print("Now returning to the Main Menu.")
+        # else:
+        #     print("Returning to the Main Menu...\n")
 
 
 if __name__ == "__main__":
