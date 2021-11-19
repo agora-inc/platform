9eebd36f812e6648b94a65b7104f5acd83942c3a
AlainRossier <alain6rossier@gmail.com>
2021-11-19 00:46:06 +0000

    profile service

--- frontend/src/Services/ProfileService.ts
+++ frontend/src/Services/ProfileService.ts
@@ -1,11 +1,68 @@
 import { baseApiUrl } from "../config";
 import axios from "axios";
-import { post } from "../Middleware/httpMiddleware";
+import { get, post } from "../Middleware/httpMiddleware";
 import { Topic } from "./TopicService";
 import { User } from "./UserService";
 
 const getAllPublicProfiles = (callback: any) => {
+  get(`profile/public`, callback);
+}
 
+const getProfile = (userId: number, callback: any) => {
+  get(`profile/id=${userId}`, callback);
+}
+
+const updateProfile = (
+  userId: number,
+  open_give_talk: boolean,
+  twitter_handle: string,
+  topic_id_1: number,
+  topic_id_2: number,
+  topic_id_3: number,
+  callback: any,
+) => {
+  post(
+    "profile/update",
+    {
+      user_id: userId,
+      open_give_talk: open_give_talk,
+      twitter_handle: twitter_handle,
+      topic_id_1: topic_id_1,
+      topic_id_2: topic_id_2,
+      topic_id_3: topic_id_3,
+    },
+    callback
+  );
+}
+
+const getPapers = (userId: number, callback: any) => {
+  get(`profile/papers/id=${userId}`, callback);
+}
+
+const updatePapers = (userId: number, papers: Paper[], callback: any) => {
+  post(
+    "profile/papers/update",
+    {
+      user_id: userId,
+      papers: papers,
+    },
+    callback
+  );
+}
+
+const getTags = (userId: number, callback: any) => {
+  get(`profile/tags/id=${userId}`, callback);
+}
+
+const updateTags = (userId: number, tags: string[], callback: any) => {
+  post(
+    "profile/tags/update",
+    {
+      user_id: userId,
+      tags: tags,
+    },
+    callback
+  );
 }
 
 export type Profile = {
@@ -19,6 +76,20 @@ export type Profile = {
   twitter_username: string
 };
 
+export type Paper = {
+  title: string;
+  authors: string;
+  publisher: string;
+  link: string;
+  number: number;
+}
+
 export const ProfileService = {
   getAllPublicProfiles,
+  getProfile,
+  updateProfile,
+  getPapers,
+  updatePapers,
+  getTags,
+  updateTags,
 };
