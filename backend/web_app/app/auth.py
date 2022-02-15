72cd68c76177cad1a5d91fd12bd9f5e42e84a3aa
Max Taylor-Davies <maxtaylordavies@gmail.com>
2022-02-15 19:34:08 +0000

    adds code to check auth0 jwt tokens + adds requires_auth decorator

--- backend/web_app/app/auth.py
+++ backend/web_app/app/auth.py
@@ -1,6 +1,16 @@
-from flask import jsonify
+from functools import wraps
+import json
+import requests
+
+from flask import jsonify, request, _request_ctx_stack
+import jwt
 
 from app import app
+
+
+AUTH0_DOMAIN = "mora-stream.eu.auth0.com"
+API_AUDIENCE = "https://mora.stream/api"
+ALGORITHMS = ["RS256"]
 
 
 class AuthError(Exception):
@@ -14,3 +24,115 @@ def handle_auth_error(ex):
     response = jsonify(ex.error)
     response.status_code = ex.status_code
     return response
+
+
+def get_token_auth_header():
+    """Obtains the Access Token from the Authorization Header"""
+    auth = request.headers.get("Authorization", None)
+    if not auth:
+        raise AuthError(
+            {
+                "code": "authorization_header_missing",
+                "description": "Authorization header is expected",
+            },
+            401,
+        )
+
+    parts = auth.split()
+
+    if parts[0].lower() != "bearer":
+        raise AuthError(
+            {
+                "code": "invalid_header",
+                "description": "Authorization header must start with" " Bearer",
+            },
+            401,
+        )
+    elif len(parts) == 1:
+        raise AuthError(
+            {"code": "invalid_header", "description": "Token not found"}, 401
+        )
+    elif len(parts) > 2:
+        raise AuthError(
+            {
+                "code": "invalid_header",
+                "description": "Authorization header must be" " Bearer token",
+            },
+            401,
+        )
+
+    token = parts[1]
+    return token
+
+
+def requires_auth(f):
+    """Determines if the Access Token is valid"""
+
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        # extract auth token from headers
+        token = get_token_auth_header()
+
+        # get keys
+        r = requests.get("https://" + AUTH0_DOMAIN + "/.well-known/jwks.json")
+        jwks = r.json()
+
+        unverified_header = jwt.get_unverified_header(token)
+        rsa_key = {}
+        for key in jwks["keys"]:
+            if key["kid"] == unverified_header["kid"]:
+                rsa_key = {
+                    "kty": key["kty"],
+                    "kid": key["kid"],
+                    "use": key["use"],
+                    "n": key["n"],
+                    "e": key["e"],
+                }
+
+        if rsa_key:
+            try:
+                payload = jwt.decode(
+                    token,
+                    rsa_key,
+                    algorithms=ALGORITHMS,
+                    audience=API_AUDIENCE,
+                    issuer="https://" + AUTH0_DOMAIN + "/",
+                )
+            except jwt.ExpiredSignatureError:
+                raise AuthError(
+                    {"code": "token_expired", "description": "token is expired"}, 401
+                )
+            except jwt.InvalidAudienceError:
+                raise AuthError(
+                    {
+                        "code": "invalid_audience",
+                        "description": "'aud' claim does not match expected value",
+                    },
+                    401,
+                )
+            except jwt.InvalidIssuerError:
+                raise AuthError(
+                    {
+                        "code": "invalid_issuer",
+                        "description": "'iss' claim does not match expected value",
+                    },
+                    401,
+                )
+            except Exception:
+                raise AuthError(
+                    {
+                        "code": "invalid_header",
+                        "description": "Unable to parse authentication" " token.",
+                    },
+                    401,
+                )
+
+            _request_ctx_stack.top.current_user = payload
+            return f(*args, **kwargs)
+
+        raise AuthError(
+            {"code": "invalid_header", "description": "Unable to find appropriate key"},
+            401,
+        )
+
+    return decorated
