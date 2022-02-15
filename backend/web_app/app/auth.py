1241d7620e586eaf8dfd49023d2cbcfc6eafb178
Max Taylor-Davies <maxtaylordavies@gmail.com>
2022-02-15 18:27:23 +0000

    creates error handler for auth errors

---
+++ backend/web_app/app/auth.py
@@ -0,0 +1,16 @@
+from flask import jsonify
+
+from app import app
+
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
