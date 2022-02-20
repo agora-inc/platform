6600b038e5dfd4ed52e2107db27eec0989bb424b
Max Taylor-Davies <maxtaylordavies@gmail.com>
2022-02-20 20:28:34 +0000

    creates new derived class for app based on Flask class, so we can initialise all the repository objects automatically within __init__

--- backend/web_app/app/__init__.py
+++ backend/web_app/app/__init__.py
@@ -28,38 +28,47 @@ from repository import (
 )
 
 
-# initialise app object
-app = Flask(__name__)
+class MoraApp(Flask):
+    def __init__(self, import_name):
+        Flask.__init__(self, import_name)
+
+        # initialise repository objects
+        self.channel_repo = ChannelRepository.ChannelRepository()
+        self.channel_sub_repo = (
+            ChannelSubscriptionRepository.ChannelSubscriptionRepository()
+        )
+        self.credit_repo = CreditRepository.CreditRepository()
+        self.email_reminders_repo = EmailRemindersRepository.EmailRemindersRepository()
+        self.invited_users_repo = InvitedUsersRepository.InvitedUsersRepository()
+        self.mailing_repo = MailingListRepository.MailingListRepository()
+        self.payments_repo = PaymentHistoryRepository.PaymentHistoryRepository()
+        self.products_repo = ProductRepository.ProductRepository()
+        self.profile_repo = ProfileRepository.ProfileRepository()
+        self.questions_repo = QandARepository.QandARepository()
+        self.user_repo = UserRepository.UserRepository()
+        self.scraper_repo = RSScraperRepository.RSScraperRepository()
+        self.search_repo = SearchRepository.SearchRepository()
+        self.stream_repo = StreamRepository.StreamRepository()
+        self.tag_repo = TagRepository.TagRepository()
+        self.talk_repo = TalkRepository.TalkRepository()
+        self.twitter_bot_repo = TwitterBotRepository.TwitterBotRepository()
+        self.topic_repo = TopicRepository.TopicRepository()
+        self.video_repo = VideoRepository.VideoRepository()
+
+        # initialise mail config
+        self.config["MAIL_SERVER"] = "smtp.office365.com"
+        self.config["MAIL_PORT"] = 587
+        self.config["MAIL_USERNAME"] = "team@agora.stream"
+        self.config["MAIL_PASSWORD"] = "123.qwe.asd"
+        self.config["MAIL_USE_TLS"] = True
+        self.config["MAIL_USE_SSL"] = False
+
+
+# create app object
+app = MoraApp(__name__)
 CORS(app)
 
-# initialise repository objects and add to app
-app.channel_repo = ChannelRepository.ChannelRepository()
-app.channel_sub_repo = ChannelSubscriptionRepository.ChannelSubscriptionRepository()
-app.credit_repo = CreditRepository.CreditRepository()
-app.email_reminders_repo = EmailRemindersRepository.EmailRemindersRepository()
-app.invited_users_repo = InvitedUsersRepository.InvitedUsersRepository()
-app.mailing_repo = MailingListRepository.MailingListRepository()
-app.payments_repo = PaymentHistoryRepository.PaymentHistoryRepository()
-app.products_repo = ProductRepository.ProductRepository()
-app.profile_repo = ProfileRepository.ProfileRepository()
-app.questions_repo = QandARepository.QandARepository()
-app.user_repo = UserRepository.UserRepository()
-app.scraper_repo = RSScraperRepository.RSScraperRepository()
-app.search_repo = SearchRepository.SearchRepository()
-app.stream_repo = StreamRepository.StreamRepository()
-app.tag_repo = TagRepository.TagRepository()
-app.talk_repo = TalkRepository.TalkRepository()
-app.twitter_bot_repo = TwitterBotRepository.TwitterBotRepository()
-app.topic_repo = TopicRepository.TopicRepository()
-app.video_repo = VideoRepository.VideoRepository()
-
-# initialise mail
-app.config["MAIL_SERVER"] = "smtp.office365.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USERNAME"] = "team@agora.stream"
-app.config["MAIL_PASSWORD"] = "123.qwe.asd"
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USE_SSL"] = False
+# create mail object
 mail = Mail(app)
 
 # logging.basicConfig(
