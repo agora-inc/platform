979e416f4d95432b9775b6135204867fee6d627b
Max Taylor-Davies <maxtaylordavies@gmail.com>
2022-02-21 23:01:32 +0000

    oops

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,10 +7,10 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Auth0Provider
-    domain="jikji.eu.auth0.com"
+    domain="mora-stream.eu.auth0.com"
     clientId="bjCm1jlXaTpPuf7WefkzJy88qrDQrNg9"
     redirectUri={window.location.origin}
-    audience="https://jikji.club/api"
+    audience="https://mora.stream/api"
   >
     <App />
   </Auth0Provider>,
