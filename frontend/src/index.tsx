c92b8a8cf6fcd4d28f7aa2f805d7abbb26f7d681
Max Taylor-Davies <maxtaylordavies@gmail.com>
2022-02-21 23:01:10 +0000

    wraps <App/> component with Auth0Provider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+  <Auth0Provider
+    domain="jikji.eu.auth0.com"
+    clientId="bjCm1jlXaTpPuf7WefkzJy88qrDQrNg9"
+    redirectUri={window.location.origin}
+    audience="https://jikji.club/api"
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
