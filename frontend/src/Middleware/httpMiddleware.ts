1da5aa704de6ecd2f60c4b60e27e65ae597dbd02
Max Taylor-Davies <maxtaylordavies@gmail.com>
2022-02-28 21:25:40 +0000

    rewrites functions in httpMiddleware.ts to accept token as param

--- frontend/src/Middleware/httpMiddleware.ts
+++ frontend/src/Middleware/httpMiddleware.ts
@@ -1,29 +1,7 @@
-import axios from "axios";
+import axios, { AxiosPromise } from "axios";
 import { baseApiUrl } from "../config";
-import { UserService } from "../Services/UserService";
 
-const parseJwt = (token: string) => {
-  var base64Url = token.split(".")[1];
-  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-  var jsonPayload = decodeURIComponent(
-    atob(base64)
-      .split("")
-      .map(function (c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-      })
-      .join("")
-  );
-
-  return JSON.parse(jsonPayload);
-};
-
-const isTokenExpired = (token: string) => {
-  let parsed = parseJwt(token);
-  let now = new Date().getTime() / 1000;
-  return now > parsed.exp;
-};
-
-const getAxiosRequestConfig = (token: string) => {
+const getRequestHeaders = (token: string) => {
   return {
     headers: {
       "Access-Control-Allow-Origin": "*",
@@ -32,83 +10,106 @@ const getAxiosRequestConfig = (token: string) => {
   };
 };
 
-export const get = async (
-  url: string,
-  onResponse: Function,
-  onError?: Function
-) => {
-  let user = localStorage.getItem("user");
-  let userObj = { id: -1, accessToken: "", refreshToken: "" };
-  let tokenExpired = true;
-
-  if (user !== null) {
-    userObj = JSON.parse(user);
-    let accessToken = userObj.accessToken;
-    if (accessToken) {
-      tokenExpired = isTokenExpired(accessToken);
-    }
-  }
-
-  if (user !== null && tokenExpired) {
-    let response = await axios.post(
-      `${baseApiUrl}/refreshtoken`,
-      { userId: userObj.id },
-      getAxiosRequestConfig(userObj.refreshToken)
-    );
-
-    let accessToken = response.data.accessToken;
-    userObj.accessToken = accessToken;
-    localStorage.setItem("user", JSON.stringify(userObj));
-  }
-
-  axios
-    .get(`${baseApiUrl}/${url}`, getAxiosRequestConfig(userObj.accessToken))
-    .then((response) => {
-      onResponse(response.data);
-    })
-    .catch((error) => {
-      onError && onError(error);
-    });
+export const get = (url: string, token: string): AxiosPromise => {
+  return axios.get(`${baseApiUrl}/api/${url}`, {
+    headers: getRequestHeaders(token),
+  });
 };
 
-export const post = async (
+export const post = (
   url: string,
   data: object,
-  onResponse: Function,
-  onError?: Function
-) => {
-  let user = localStorage.getItem("user");
-  let userObj = { id: -1, accessToken: "", refreshToken: "" };
-  let tokenExpired = true;
-
-  if (user !== null) {
-    userObj = JSON.parse(user);
-    let accessToken = userObj.accessToken;
-    tokenExpired = isTokenExpired(accessToken);
-  }
-
-  if (user !== null && tokenExpired) {
-    let response = await axios.post(
-      `${baseApiUrl}/refreshtoken`,
-      { userId: userObj.id },
-      getAxiosRequestConfig(userObj.refreshToken)
-    );
-
-    let accessToken = response.data.accessToken;
-    userObj.accessToken = accessToken;
-    localStorage.setItem("user", JSON.stringify(userObj));
-  }
-
-  axios
-    .post(
-      `${baseApiUrl}/${url}`,
-      data,
-      getAxiosRequestConfig(userObj.accessToken)
-    )
-    .then((response) => {
-      onResponse(response.data);
-    })
-    .catch((error) => {
-      onError && onError(error);
-    });
+  token: string
+): AxiosPromise => {
+  return axios.post(`${baseApiUrl}/api/${url}`, data, {
+    headers: getRequestHeaders(token),
+  });
 };
+
+export const del = (url: string, data: object, token: string): AxiosPromise => {
+  return axios.delete(`${baseApiUrl}/api/${url}`, {
+    data,
+    headers: getRequestHeaders(token),
+  });
+};
+
+// export const get = async (
+//   url: string,
+//   onResponse: Function,
+//   onError?: Function
+// ) => {
+//   let user = localStorage.getItem("user");
+//   let userObj = { id: -1, accessToken: "", refreshToken: "" };
+//   let tokenExpired = true;
+
+//   if (user !== null) {
+//     userObj = JSON.parse(user);
+//     let accessToken = userObj.accessToken;
+//     if (accessToken) {
+//       tokenExpired = isTokenExpired(accessToken);
+//     }
+//   }
+
+//   if (user !== null && tokenExpired) {
+//     let response = await axios.post(
+//       `${baseApiUrl}/refreshtoken`,
+//       { userId: userObj.id },
+//       getAxiosRequestConfig(userObj.refreshToken)
+//     );
+
+//     let accessToken = response.data.accessToken;
+//     userObj.accessToken = accessToken;
+//     localStorage.setItem("user", JSON.stringify(userObj));
+//   }
+
+//   axios
+//     .get(`${baseApiUrl}/${url}`, getAxiosRequestConfig(userObj.accessToken))
+//     .then((response) => {
+//       onResponse(response.data);
+//     })
+//     .catch((error) => {
+//       onError && onError(error);
+//     });
+// };
+
+// export const post = async (
+//   url: string,
+//   data: object,
+//   onResponse: Function,
+//   onError?: Function
+// ) => {
+//   let user = localStorage.getItem("user");
+//   let userObj = { id: -1, accessToken: "", refreshToken: "" };
+//   let tokenExpired = true;
+
+//   if (user !== null) {
+//     userObj = JSON.parse(user);
+//     let accessToken = userObj.accessToken;
+//     tokenExpired = isTokenExpired(accessToken);
+//   }
+
+//   if (user !== null && tokenExpired) {
+//     let response = await axios.post(
+//       `${baseApiUrl}/refreshtoken`,
+//       { userId: userObj.id },
+//       getAxiosRequestConfig(userObj.refreshToken)
+//     );
+
+//     let accessToken = response.data.accessToken;
+//     userObj.accessToken = accessToken;
+//     localStorage.setItem("user", JSON.stringify(userObj));
+//   }
+
+//   axios
+//     .post(
+//       `${baseApiUrl}/${url}`,
+//       data,
+//       getAxiosRequestConfig(userObj.accessToken)
+//     )
+//     .then((response) => {
+//       onResponse(response.data);
+//     })
+//     .catch((error) => {
+//       onError && onError(error);
+//     });
+// };
