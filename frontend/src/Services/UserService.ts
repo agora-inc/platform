dc70a103c7289064d2bcdd2de12bd0cd8218736b
Max Taylor-Davies <maxtaylordavies@gmail.com>
2022-03-02 23:38:50 +0000

    same for UserService and VideoService

--- frontend/src/Services/UserService.ts
+++ frontend/src/Services/UserService.ts
@@ -1,8 +1,6 @@
-import { baseApiUrl } from "../config";
-import axios from "axios";
 import { User as Auth0UserType } from "@auth0/auth0-spa-js";
 
-import { post } from "../Middleware/httpMiddleware";
+import { get, post } from "../Middleware/httpMiddleware";
 
 type DatabaseUser = {
   // database fields
@@ -41,151 +39,158 @@ const createUserObjectFromAuth0Payload = (payload: Auth0UserPayload): User => {
 };
 
 const getAllPublicUsers = (callback: any) => {
-  axios
-    .get(baseApiUrl + "/users/public")
-    .then((response) => {
-      callback(response.data);
-    })
-    .catch((error) => console.error(error));
+  get("users/public", "", callback);
 };
 
-const register = (
-  username: string,
-  password: string,
-  email: string,
-  position: string,
-  institution: string,
-  refChannel: number,
-  callback: any
+// const register = (
+//   username: string,
+//   password: string,
+//   email: string,
+//   position: string,
+//   institution: string,
+//   refChannel: number,
+//   callback: any
+// ) => {
+//   axios
+//     .post(
+//       baseApiUrl + "/users/add",
+//       {
+//         username: username,
+//         password: password,
+//         email: email,
+//         position: position,
+//         institution: institution,
+//         refChannel: refChannel,
+//       },
+//       { headers: { "Access-Control-Allow-Origin": "*" } }
+//     )
+//     .then(function (response) {
+//       localStorage.setItem("user", JSON.stringify(response.data));
+//       callback({ status: "ok", userId: response.data.id });
+//       // getting weird error about expecting no arguments
+//       window.location.reload();
+//     })
+//     .catch(function (error) {
+//       callback({ status: error.response.data, userId: -1 });
+//     });
+// };
+
+// const login = (credential: string, password: string, callback: any) => {
+//   axios
+//     .post(
+//       baseApiUrl + "/users/authenticate",
+//       { credential: credential, password: password },
+//       { headers: { "Access-Control-Allow-Origin": "*" } }
+//     )
+//     .then(function (response) {
+//       localStorage.setItem("user", JSON.stringify(response.data));
+//       callback(true);
+//       window.location.reload();
+//     })
+//     .catch(function (error) {
+//       callback(false);
+//     });
+// };
+
+// const logout = () => {
+//   localStorage.removeItem("user");
+//   window.location.reload();
+// };
+
+// const isLoggedIn = () => {
+//   const user = localStorage.getItem("user");
+//   return user !== null;
+// };
+
+// const getCurrentUser = () => {
+//   const user = localStorage.getItem("user");
+//   return user ? JSON.parse(user) : null;
+// };
+
+// const emailChangePasswordLink = (usernameOrEmail: string, callback: any) => {
+//   axios
+//     .post(
+//       baseApiUrl + "/users/email_change_password_link",
+//       { usernameOrEmail },
+//       {
+//         headers: {
+//           "Access-Control-Allow-Origin": "*",
+//         },
+//       }
+//     )
+//     .then(function (response) {
+//       // localStorage.setItem("user", JSON.stringify(response.data));
+//       callback(true);
+//     })
+//     .catch(function (error) {
+//       callback(false);
+//     });
+// };
+
+// const changePassword = (newPassword: string, code: string, callback: any) => {
+//   axios
+//     .post(
+//       baseApiUrl + "/users/change_password",
+//       { password: newPassword },
+//       {
+//         headers: {
+//           "Access-Control-Allow-Origin": "*",
+//           Authorization: `Bearer ${code}`,
+//         },
+//       }
+//     )
+//     .then(function (response) {
+//       // localStorage.setItem("user", JSON.stringify(response.data));
+//       callback(true);
+//     })
+//     .catch(function (error) {
+//       callback(false);
+//     });
+// };
+
+const updateBio = (
+  userId: number,
+  newBio: string,
+  callback: any,
+  token: string
 ) => {
-  axios
-    .post(
-      baseApiUrl + "/users/add",
-      {
-        username: username,
-        password: password,
-        email: email,
-        position: position,
-        institution: institution,
-        refChannel: refChannel,
-      },
-      { headers: { "Access-Control-Allow-Origin": "*" } }
-    )
-    .then(function (response) {
-      localStorage.setItem("user", JSON.stringify(response.data));
-      callback({ status: "ok", userId: response.data.id });
-      // getting weird error about expecting no arguments
-      window.location.reload();
-    })
-    .catch(function (error) {
-      callback({ status: error.response.data, userId: -1 });
-    });
-};
-
-const login = (credential: string, password: string, callback: any) => {
-  axios
-    .post(
-      baseApiUrl + "/users/authenticate",
-      { credential: credential, password: password },
-      { headers: { "Access-Control-Allow-Origin": "*" } }
-    )
-    .then(function (response) {
-      localStorage.setItem("user", JSON.stringify(response.data));
-      callback(true);
-      window.location.reload();
-    })
-    .catch(function (error) {
-      callback(false);
-    });
-};
-
-const logout = () => {
-  localStorage.removeItem("user");
-  window.location.reload();
-};
-
-const isLoggedIn = () => {
-  const user = localStorage.getItem("user");
-  return user !== null;
-};
-
-const getCurrentUser = () => {
-  const user = localStorage.getItem("user");
-  return user ? JSON.parse(user) : null;
-};
-
-const emailChangePasswordLink = (usernameOrEmail: string, callback: any) => {
-  axios
-    .post(
-      baseApiUrl + "/users/email_change_password_link",
-      { usernameOrEmail },
-      {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
-      }
-    )
-    .then(function (response) {
-      // localStorage.setItem("user", JSON.stringify(response.data));
-      callback(true);
-    })
-    .catch(function (error) {
-      callback(false);
-    });
-};
-
-const changePassword = (newPassword: string, code: string, callback: any) => {
-  axios
-    .post(
-      baseApiUrl + "/users/change_password",
-      { password: newPassword },
-      {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          Authorization: `Bearer ${code}`,
-        },
-      }
-    )
-    .then(function (response) {
-      // localStorage.setItem("user", JSON.stringify(response.data));
-      callback(true);
-    })
-    .catch(function (error) {
-      callback(false);
-    });
-};
-
-const updateBio = (userId: number, newBio: string, callback: any) => {
   post(
     "users/update_bio",
     {
       userId: userId,
       newBio: newBio,
     },
+    token,
     callback
   );
 };
 
-const updatePublic = (userId: number, _public: boolean, callback: any) => {
+const updatePublic = (
+  userId: number,
+  _public: boolean,
+  callback: any,
+  token: string
+) => {
   post(
     "users/update_public",
     {
       userId,
       public: _public,
     },
+    token,
     callback
   );
 };
 
 export const UserService = {
   createUserObjectFromAuth0Payload,
-  register,
-  login,
-  logout,
-  isLoggedIn,
-  getCurrentUser,
-  changePassword,
-  emailChangePasswordLink,
+  // register,
+  // login,
+  // logout,
+  // isLoggedIn,
+  // getCurrentUser,
+  // changePassword,
+  // emailChangePasswordLink,
   updateBio,
   updatePublic,
 };
